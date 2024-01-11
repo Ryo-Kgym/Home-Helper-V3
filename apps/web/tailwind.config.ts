9f44b9b7766f7500754ad5f21657ab0e9323fa58
Ryo-Kgym <mraytoh@icloud.com>
2024-01-11 12:45:29 +0900

    設定変更

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -5,12 +5,24 @@
 // tailwind config is required for editor support
 
 import type { Config } from "tailwindcss";
-// @ts-ignore
-import sharedConfig from "@repo/tailwind-config";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
-const config: Pick<Config, "content" | "presets"> = {
-  content: ["./src/app/**/*.tsx", "./src/components/**/*.{js,ts,jsx,tsx}"],
-  presets: [sharedConfig],
-};
+import baseConfig from "@acme/tailwind-config";
 
-export default config;
+export default {
+  content: [
+    "./src/app/**/*.tsx",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    ...baseConfig.content,
+    "../../packages/ui/**/*.{ts,tsx}",
+  ],
+  presets: [baseConfig],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        mono: ["var(--font-geist-mono)", ...fontFamily.mono],
+      },
+    },
+  },
+} satisfies Config;
