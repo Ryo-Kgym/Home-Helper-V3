d032f2ecf24a1c1ffada92676de4bc9c3bec726b
Ryo-Kgym <mraytoh@icloud.com>
2024-01-13 14:09:52 +0900

    Home-Helper-V3-12 - urqlを使用する設定

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { datasource } from "@/turbo/graphql";
+import { Provider as UrqlProvider } from "urql";
 
 import "../styles.css";
 
 const Layout = () => (
-  <>
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  <UrqlProvider value={datasource}>
     <Stack
       screenOptions={{
         headerStyle: {
@@ -13,6 +16,6 @@ const Layout = () => (
       }}
     />
     <StatusBar />
-  </>
+  </UrqlProvider>
 );
 export default Layout;
