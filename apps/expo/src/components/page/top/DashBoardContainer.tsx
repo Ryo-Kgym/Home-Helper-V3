7a27d2e1d518ae46288e1327007eda6d39622f47
Ryo-Kgym <mraytoh@icloud.com>
2024-01-13 15:50:20 +0900

    Home-Helper-V3-12 - データ取得のサンプル作成完了

--- apps/expo/src/components/page/top/DashBoardContainer.tsx
+++ apps/expo/src/components/page/top/DashBoardContainer.tsx
@@ -1,5 +1,23 @@
+import { useGetAccountBalanceListQuery } from "@/turbo/graphql/household";
+
+import { mock } from "~/config/mock";
 import { DashBoardPresenter } from "./DashBoardPresenter";
 
 export const DashBoardContainer = () => {
-  return <DashBoardPresenter />;
+  const [{ data }] = useGetAccountBalanceListQuery({
+    variables: {
+      groupId: mock.groupId,
+      fromDate: "2019-01-01",
+      toDate: "2024-12-31",
+    },
+  });
+  const accounts =
+    data?.account.map((account) => ({
+      id: account.id,
+      name: account.accountName,
+      balance:
+        (account.allDetailViewsAggregate?.aggregate?.sum
+          ?.signedAmount as number) ?? 0,
+    })) ?? [];
+  return <DashBoardPresenter accounts={accounts} />;
 };
--- apps/expo/src/components/page/top/DashBoardPresenter.tsx
+++ apps/expo/src/components/page/top/DashBoardPresenter.tsx
@@ -1,28 +1,26 @@
-import { Button, Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
 
-export const DashBoardPresenter = () => {
-  return (
-    <SafeAreaView className="bg-[#000000]">
-      <Stack.Screen options={{ title: "Top" }} />
-      <View className="h-full w-full p-4">
-        <Text className="pb-2 text-center text-5xl font-bold text-white">
-          Create <Text className="text-pink-400">T3</Text> Turbo
-        </Text>
-
-        <Button
-          onPress={() => undefined}
-          title="Refresh posts"
-          color={"#729bf4"}
-        />
-
-        <View className="py-2">
-          <Text className="font-semibold italic text-black">
-            Press on a post
-          </Text>
-        </View>
-      </View>
-    </SafeAreaView>
-  );
-};
+export const DashBoardPresenter = ({
+  accounts,
+}: {
+  accounts: { id: string; name: string; balance: number }[];
+}) => (
+  <SafeAreaView>
+    <Stack.Screen options={{ title: "Top" }} />
+    <View className="">
+      <FlatList
+        data={accounts}
+        renderItem={(a) => (
+          <View key={a.item.id} className="flex-row px-4 text-black">
+            <Text className="w-2/3 text-xl">{a.item.name}</Text>
+            <Text className="w-1/3 text-right text-xl">
+              {a.item.balance.toLocaleString()}
+            </Text>
+          </View>
+        )}
+      />
+    </View>
+  </SafeAreaView>
+);
