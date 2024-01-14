a9eed96efef7ff9f1b8f75ecc9c75667468974b9
Ryo-Kgym <mraytoh@icloud.com>
2024-01-14 15:11:53 +0900

    Home-Helper-V3-12 - カレンダーの日付がづれていたので修正した。

--- apps/expo/src/components/page/household/calendar/generate-calendar.ts
+++ apps/expo/src/components/page/household/calendar/generate-calendar.ts
@@ -1,12 +1,12 @@
-export const generateCalendar = (date: Date): Date[] => {
-  const dateList = [];
-  const year = date.getFullYear();
-  const month = date.getMonth();
-  const firstDay = new Date(year, month, 1).getDay();
+export const generateCalendar = (baseDate: Date): Date[] => {
+  const dateList: Date[] = [];
+  const year = baseDate.getFullYear();
+  const month = baseDate.getMonth();
+  const firstWeek = new Date(year, month, 1).getDay() - 2;
   const count = 7 * 6;
 
   for (let i = 0; i < count; i++) {
-    const date = new Date(year, month, i - firstDay + 1);
+    const date = new Date(year, month, i - firstWeek);
     dateList.push(date);
   }
 
