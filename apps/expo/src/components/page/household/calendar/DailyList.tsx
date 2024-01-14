c20a55b0b485dfa37e09de936001c362ee97bb62
Ryo-Kgym <mraytoh@icloud.com>
2024-01-14 14:55:57 +0900

    Home-Helper-V3-12 - 日次のリストを作成した。

--- apps/expo/src/components/page/household/calendar/DailyList.tsx
+++ apps/expo/src/components/page/household/calendar/DailyList.tsx
@@ -13,11 +13,20 @@ export const DailyList = ({
 }) => (
   <View>
     {details.map((detail) => (
-      <View key={detail.id} className={"flex-row"}>
-        <Text>{detail.accountName}</Text>
-        <Text>{detail.amount.toLocaleString()}円</Text>
-        <Text>{detail.categoryName}</Text>
-        <Text>{detail.genreName}</Text>
+      <View
+        key={detail.id}
+        className={"flex-row items-center border-b border-b-gray-400 px-3 py-2"}
+      >
+        <View className={"w-2/3 "}>
+          <Text className={"text-xl"}>{detail.categoryName}</Text>
+          <View className={"flex-row items-center"}>
+            <Text className={"text-gray-600"}>{detail.genreName + "  "}</Text>
+            <Text className={"text-gray-600"}>{detail.accountName}</Text>
+          </View>
+        </View>
+        <Text className={"w-1/3 text-right text-xl"}>
+          {detail.amount.toLocaleString()}円
+        </Text>
       </View>
     ))}
   </View>
