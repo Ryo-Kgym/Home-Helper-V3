a0612eed6070e45e45b07dbc21cf555123124837
Ryo-Kgym <mraytoh@icloud.com>
2024-01-14 15:05:17 +0900

    Home-Helper-V3-12 - FlatListを使用するようにした。

--- apps/expo/src/components/page/household/calendar/DailyList.tsx
+++ apps/expo/src/components/page/household/calendar/DailyList.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
 
 export const DailyList = ({
   details,
@@ -12,22 +12,29 @@ export const DailyList = ({
   }[];
 }) => (
   <View>
-    {details.map((detail) => (
-      <View
-        key={detail.id}
-        className={"flex-row items-center border-b border-b-gray-400 px-3 py-2"}
-      >
-        <View className={"w-2/3 "}>
-          <Text className={"text-xl"}>{detail.categoryName}</Text>
-          <View className={"flex-row items-center"}>
-            <Text className={"text-gray-600"}>{detail.genreName + "  "}</Text>
-            <Text className={"text-gray-600"}>{detail.accountName}</Text>
+    <FlatList
+      data={details}
+      keyExtractor={(item) => item.id}
+      renderItem={(detail) => (
+        <View
+          className={
+            "flex-row items-center border-b border-b-gray-400 px-3 py-2"
+          }
+        >
+          <View className={"w-2/3 "}>
+            <Text className={"text-xl"}>{detail.item.categoryName}</Text>
+            <View className={"flex-row items-center"}>
+              <Text className={"text-gray-600"}>
+                {detail.item.genreName + "  "}
+              </Text>
+              <Text className={"text-gray-600"}>{detail.item.accountName}</Text>
+            </View>
           </View>
+          <Text className={"w-1/3 text-right text-xl"}>
+            {detail.item.amount.toLocaleString()}円
+          </Text>
         </View>
-        <Text className={"w-1/3 text-right text-xl"}>
-          {detail.amount.toLocaleString()}円
-        </Text>
-      </View>
-    ))}
+      )}
+    />
   </View>
 );
