da04aa82248d80d663ed00062c8356cc9993403a
Ryo-Kgym <mraytoh@icloud.com>
2024-01-15 21:43:41 +0900

    Home-Helper-V3-12 - 符号は不要なので、削除する。

--- apps/expo/src/components/page/household/dashboard/MonthlyBox.tsx
+++ apps/expo/src/components/page/household/dashboard/MonthlyBox.tsx
@@ -22,7 +22,7 @@ export const MonthlyBox = () => {
         <View className={"flex-row items-center"}>
           <Text className={"w-1/3 text-center"}>支出</Text>
           <Text className={"w-2/3 text-right text-3xl text-red-500"}>
-            {(-1 * outcomeTotal).toLocaleString()}
+            {outcomeTotal.toLocaleString()}
           </Text>
         </View>
         <View className={"flex-row items-center"}>
