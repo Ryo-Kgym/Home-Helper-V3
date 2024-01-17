a59b00e54955faac8bb9d7b9b9bdd226c9700a8e
Ryo-Kgym <mraytoh@icloud.com>
2024-01-17 12:49:38 +0900

    Home-Helper-V3-16 - 除外を追加

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -33,6 +33,7 @@ const config = {
     "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
   },
   ignorePatterns: [
     "**/hasuraGraphql.tsx",
