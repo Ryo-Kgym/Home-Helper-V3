a9f0ee11407b3337ebc7d6d6695574d00bac09fe
Ryo-Kgym <mraytoh@icloud.com>
2024-01-17 15:20:15 +0900

    Home-Helper-V3-16 - フィールドのフレームを作成した。

--- apps/expo/src/ui/editable/EditableAmount.tsx
+++ apps/expo/src/ui/editable/EditableAmount.tsx
@@ -2,6 +2,7 @@ import { TextInput } from "react-native";
 
 import type { IocomeType } from "~/types/iocome-type";
 import type { EditableProps } from "~/ui/editable/editable-props";
+import { FiledFrame } from "~/ui/FiledFrame";
 import { Amount } from "../Amount";
 
 export const EditableAmount = ({
@@ -12,21 +13,21 @@ export const EditableAmount = ({
   disabled = false,
   defaultValue,
 }: EditableProps<number> & { iocomeType: IocomeType }) => {
-  if (disabled) {
-    return (
-      <Amount className={className} amount={value} iocomeType={iocomeType} />
-    );
-  }
-
   return (
-    <TextInput
-      className={`${className} rounded-xl border-2 border-gray-200 bg-gray-100 p-2`}
-      onChange={(e) => setValue(Number(e.nativeEvent.text))}
-      editable={!disabled}
-      inputMode={"numeric"}
-      placeholder={String(defaultValue)}
-    >
-      {value}
-    </TextInput>
+    <FiledFrame>
+      {disabled ? (
+        <Amount className={className} amount={value} iocomeType={iocomeType} />
+      ) : (
+        <TextInput
+          className={className}
+          onChange={(e) => setValue(Number(e.nativeEvent.text))}
+          editable={!disabled}
+          inputMode={"numeric"}
+          placeholder={String(defaultValue)}
+        >
+          {value}
+        </TextInput>
+      )}
+    </FiledFrame>
   );
 };
--- apps/expo/src/ui/editable/EditableMemo.tsx
+++ apps/expo/src/ui/editable/EditableMemo.tsx
@@ -1,6 +1,7 @@
 import { TextInput } from "react-native";
 
 import type { EditableProps } from "~/ui/editable/editable-props";
+import { FiledFrame } from "~/ui/FiledFrame";
 
 export const EditableMemo = ({
   value,
@@ -10,16 +11,18 @@ export const EditableMemo = ({
   defaultValue,
 }: EditableProps<string | null>) => {
   return (
-    <TextInput
-      className={`${className} rounded-xl border-2 border-gray-200 bg-gray-100 p-2`}
-      numberOfLines={4}
-      multiline={true}
-      onChangeText={setValue}
-      editable={!disabled}
-      inputMode={"text"}
-      placeholder={defaultValue ?? undefined}
-    >
-      {value}
-    </TextInput>
+    <FiledFrame>
+      <TextInput
+        className={className}
+        numberOfLines={4}
+        multiline={true}
+        onChangeText={setValue}
+        editable={!disabled}
+        inputMode={"text"}
+        placeholder={defaultValue ?? undefined}
+      >
+        {value}
+      </TextInput>
+    </FiledFrame>
   );
 };
