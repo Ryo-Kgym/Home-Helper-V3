285d53d3d28c3c191e8e61a648024cad47dfdcb8
Ryo-Kgym <mraytoh@icloud.com>
2024-01-23 14:14:05 +0900

    Home-Helper-V3-27 - filterを指定できるようにした。

--- apps/expo/src/hooks/household/detail/calc-total.test.ts
+++ apps/expo/src/hooks/household/detail/calc-total.test.ts
@@ -52,4 +52,38 @@ describe("calcTotal", () => {
       balance: -200,
     });
   });
+
+  it("フィルターを使用した場合、条件に合致するものが計算される", () => {
+    const actual = calcTotal(
+      [
+        {
+          amount: 100,
+          iocomeType: "INCOME",
+          mark: "A",
+        },
+        {
+          amount: 200,
+          iocomeType: "OUTCOME",
+          mark: "A",
+        },
+        {
+          amount: 300,
+          iocomeType: "INCOME",
+          mark: "B",
+        },
+        {
+          amount: 400,
+          iocomeType: "OUTCOME",
+          mark: "B",
+        },
+      ],
+      (data) => data.mark === "A",
+      (data) => data.mark === "A",
+    );
+    expect(actual).toEqual({
+      incomeTotal: 100,
+      outcomeTotal: 200,
+      balance: -100,
+    });
+  });
 });
