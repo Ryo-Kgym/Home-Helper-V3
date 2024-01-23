06a9af8810e2cddb1a9c8bba608413e87b920c47
Ryo-Kgym <mraytoh@icloud.com>
2024-01-24 00:23:59 +0900

    Home-Helper-V3-29 - クレジットカードサマリを表示するようにした。

--- apps/expo/src/feature/household/detailList/account/DetailListByAccountContainer.tsx
+++ apps/expo/src/feature/household/detailList/account/DetailListByAccountContainer.tsx
@@ -1,12 +1,19 @@
+import type { ComponentProps } from "react";
 import { useRouter } from "expo-router";
 import {
   OrderBy,
+  useGetCreditCardSummaryByAccountIdBetweenDateQuery,
   useGetDailyByAccountIdQuery,
 } from "@/turbo/graphql/household";
 
+import type { CreditCard } from "~/types/credit-card";
 import type { IocomeType } from "~/types/iocome-type";
 import { paths } from "~/app/paths";
 import { useSaveGroupId } from "~/hooks/group/useSaveGroupId";
+import {
+  getCreditCardGenreName,
+  getCreditCardName,
+} from "~/hooks/household/credit_card/get-label";
 import { Details } from "~/ui";
 
 export const DetailListByAccountContainer = ({
@@ -25,9 +32,16 @@ export const DetailListByAccountContainer = ({
       orderBy: OrderBy.Desc,
     },
   });
+  const [{ data: c }] = useGetCreditCardSummaryByAccountIdBetweenDateQuery({
+    variables: {
+      accountId,
+      fromDate: "2019-01-01",
+      toDate: "2099-12-31",
+    },
+  });
 
-  const details =
-    data?.dailies.map((d) => ({
+  const details: ComponentProps<typeof Details>["details"] = [
+    ...(data?.dailies.map((d) => ({
       id: d.id,
       date: d.date ? new Date(d.date) : undefined,
       accountId: d.account.id,
@@ -38,7 +52,20 @@ export const DetailListByAccountContainer = ({
       iocomeType: d.genre.iocomeType as IocomeType,
       redirectHandler: () => push(paths.household.daily(d.id) as "/"),
       memo: d.memo ?? null,
-    })) ?? [];
+    })) ?? []),
+    ...(c?.allCreditCardSummariesList.map((d) => ({
+      id: d.id,
+      date: d.withdrawalDate ? new Date(d.withdrawalDate) : undefined,
+      accountId: d.accountByAccountId.accountId,
+      accountName: d.accountByAccountId.accountName,
+      amount: (d.totalAmount as number) ?? 0,
+      categoryName: getCreditCardName(d.creditCard as CreditCard),
+      genreName: getCreditCardGenreName(),
+      iocomeType: "OUTCOME" as IocomeType,
+      redirectHandler: () => undefined,
+      memo: null,
+    })) ?? []),
+  ];
 
   return <Details details={details} />;
 };
