71db5ab2f4c32bcdcb0985266a5680c5da496450
Ryo-Kgym <mraytoh@icloud.com>
2024-01-23 23:38:42 +0900

    Home-Helper-V3-31 - カレンダーの日付の選択範囲が狭かったので広げた

--- apps/web/src/app/household/daily/@calendar/_material/day/DayPresenter.tsx
+++ apps/web/src/app/household/daily/@calendar/_material/day/DayPresenter.tsx
@@ -30,16 +30,16 @@ export const DayPresenter = ({
   saveDateHandler,
 }: DayPresenterProps) => (
   <>
-    <div
-      className={`h-32 cursor-pointer border-b-2 border-r-2 border-gray-400 hover:bg-amber-200 
+    <button
+      className={`h-32 cursor-pointer border-b-2 border-r-2 border-gray-400 bg-inherit hover:bg-amber-200
       ${isToday && "bg-amber-100"} ${isNotThisMonth && "bg-gray-300"}`}
+      onClick={saveDateHandler}
     >
-      <DayLabel openClickHandler={openClickHandler}>{displayDay}</DayLabel>
+      <div className={"h-1/3 w-1/3"}>
+        <DayLabel openClickHandler={openClickHandler}>{displayDay}</DayLabel>
+      </div>
 
-      <button
-        className={"grid grid-cols-1 bg-inherit"}
-        onClick={saveDateHandler}
-      >
+      <div className={"grid h-2/3 grid-cols-1"}>
         <TotalLine
           price={income}
           backgroundColor={`${income && "bg-green-300"} `}
@@ -48,14 +48,12 @@ export const DayPresenter = ({
           price={outcome}
           backgroundColor={`${outcome && "bg-red-300"} `}
         />
-        {income && outcome && (
-          <TotalLine
-            price={income - outcome}
-            backgroundColor={"bg-yellow-300"}
-          />
-        )}
-      </button>
-    </div>
+        <TotalLine
+          price={income && outcome ? income - outcome : undefined}
+          backgroundColor={`${income && outcome && "bg-yellow-300"}`}
+        />
+      </div>
+    </button>
     <Modal opened={opened} onClose={onClose}>
       <RegisterDailyDetail date={date} />
     </Modal>
@@ -82,8 +80,8 @@ const TotalLine = ({
   price,
   backgroundColor = "bg-inherit",
 }: {
-  price?: number;
-  backgroundColor?: string;
+  price: number | undefined;
+  backgroundColor: string | undefined;
 }) => (
   <div
     className={`m-0.5 rounded-md pr-1 ${backgroundColor} h-[1.5em] text-right`}
