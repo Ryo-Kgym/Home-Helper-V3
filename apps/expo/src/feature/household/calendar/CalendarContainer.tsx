11f929b3a599c0c5fcc540f91893ef76361057df
Ryo-Kgym <mraytoh@icloud.com>
2024-01-23 10:12:03 +0900

    Home-Helper-V3-25 - 共有コンポを使用して、らファクタリングをした。

--- apps/expo/src/feature/household/calendar/CalendarContainer.tsx
+++ apps/expo/src/feature/household/calendar/CalendarContainer.tsx
@@ -1,10 +1,8 @@
-import { useEffect, useState } from "react";
 import { View } from "react-native";
 import { useRouter } from "expo-router";
 
 import { paths } from "~/app/paths";
-import { useGetCreditCardSummaryList } from "~/hooks/household/credit_card/useGetCreditCardSummaryList";
-import { useGetDailyList } from "~/hooks/household/daily/useGetDailyList";
+import { useGetDetails } from "~/hooks/household/detail/useGetDetails";
 import { Details, Total } from "~/ui";
 import { CalendarPresenter } from "./CalendarPresenter";
 import { generateCalendar } from "./generate-calendar";
@@ -12,67 +10,59 @@ import { generateCalendar } from "./generate-calendar";
 export const CalendarContainer = ({ baseDate }: { baseDate: Date }) => {
   const { push } = useRouter();
   const today = new Date();
-  const [date, setDate] = useState<Date>(baseDate);
 
-  const days = generateCalendar(baseDate).map((day) => ({
+  const calendar = generateCalendar(baseDate);
+  const fromDate = calendar[0]!;
+  const toDate = calendar[calendar.length - 1]!;
+
+  const { getDetailsByDate } = useGetDetails({
+    fromDate,
+    toDate,
+    dailyConverter: (detail) => ({
+      id: detail.id,
+      accountName: detail.account.name,
+      amount: detail.amount,
+      genreName: detail.genre.name,
+      categoryName: detail.category.name,
+      iocomeType: detail.genre.iocomeType,
+      memo: detail.memo,
+      redirectHandler: () => push(paths.household.daily(detail.id) as "/"),
+    }),
+    creditCardSummaryConverter: (summary) => ({
+      id: summary.id,
+      accountName: summary.account.name,
+      amount: summary.total,
+      genreName: summary.genre.name,
+      categoryName: summary.category.name,
+      iocomeType: summary.genre.iocomeType,
+      memo: null,
+      redirectHandler: () => undefined,
+    }),
+  });
+  const { details, incomeTotal, outcomeTotal } = getDetailsByDate(baseDate);
+
+  const days = calendar.map((day) => ({
     date: day,
     isToday:
       day.toISOString().slice(0, 10) === today.toISOString().slice(0, 10),
     isThisMonth: day.getMonth() === baseDate.getMonth(),
     isSelectedDate:
-      date.toISOString().slice(0, 10) === day.toISOString().slice(0, 10),
+      baseDate.toISOString().slice(0, 10) === day.toISOString().slice(0, 10),
   }));
-
-  const { dailyDetailList, incomeTotal, outcomeTotal } = useGetDailyList({
-    fromDate: date,
-    toDate: date,
-  });
-
-  const { creditCardSummaryList } = useGetCreditCardSummaryList({
-    fromDate: date,
-    toDate: date,
-  });
 
   const changeHandler = (date: Date) =>
     push(paths.household.calendar(date) as "/");
 
-  useEffect(() => setDate(baseDate), [baseDate]);
-
   return (
     <>
       <CalendarPresenter
-        baseDate={date}
+        baseDate={baseDate}
         changeHandler={changeHandler}
         days={days}
       />
       <Total income={incomeTotal} outcome={outcomeTotal} />
       <View className={"h-[38vh]"}>
-        <Details
-          details={dailyDetailList
-            .map((detail) => ({
-              id: detail.id,
-              accountName: detail.account.name,
-              amount: detail.amount,
-              genreName: detail.genre.name,
-              categoryName: detail.category.name,
-              iocomeType: detail.genre.iocomeType,
-              memo: detail.memo,
-              redirectHandler: () =>
-                push(paths.household.daily(detail.id) as "/"),
-            }))
-            .concat(
-              creditCardSummaryList.map((summary) => ({
-                id: summary.id,
-                accountName: summary.account.name,
-                amount: summary.total,
-                genreName: summary.genre.name,
-                categoryName: summary.category.name,
-                iocomeType: summary.genre.iocomeType,
-                memo: null,
-                redirectHandler: () => undefined,
-              })),
-            )}
-        />
+        <Details details={details} />
       </View>
     </>
   );
