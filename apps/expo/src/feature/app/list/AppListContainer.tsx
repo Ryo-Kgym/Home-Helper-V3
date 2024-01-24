a8351eb2a21468116c0aaa33736ef684dfe660ff
Ryo-Kgym <mraytoh@icloud.com>
2024-01-24 12:29:35 +0900

    Home-Helper-V3-36 - Loadingを仮実装した。

--- apps/expo/src/feature/app/list/AppListContainer.tsx
+++ apps/expo/src/feature/app/list/AppListContainer.tsx
@@ -1,11 +1,21 @@
+import { Text } from "react-native";
+import { useGetAppsQuery } from "@/turbo/graphql/household";
+
 import { paths } from "~/app/paths";
+import { useSaveGroupId } from "~/hooks/group/useSaveGroupId";
 import { AppListPresenter } from "./AppListPresenter";
 
 export const AppListContainer = () => {
-  const apps = [
-    // @formatter::off
-    { appName: "家計簿アプリ", path: paths.household.dashboard },
-  ];
+  const { groupId } = useSaveGroupId();
+  const [{ data, fetching }] = useGetAppsQuery({ variables: { groupId } });
+
+  if (fetching) return <Text>Loading...</Text>;
+
+  const apps =
+    data?.group.map((g) => ({
+      appName: g.app.name,
+      path: paths.household.dashboard,
+    })) ?? [];
 
   return <AppListPresenter apps={apps} />;
 };
