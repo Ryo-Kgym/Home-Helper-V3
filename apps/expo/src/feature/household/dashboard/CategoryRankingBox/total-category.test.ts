29f5647a6581c136f8b86d6b55594614836162e9
Ryo-Kgym <mraytoh@icloud.com>
2024-01-25 19:42:58 +0900

    Home-Helper-V3-40 - フィルターを指定できるようにした。

--- apps/expo/src/feature/household/dashboard/CategoryRankingBox/total-category.test.ts
+++ apps/expo/src/feature/household/dashboard/CategoryRankingBox/total-category.test.ts
@@ -3,13 +3,13 @@ import { describe, expect, it } from "@jest/globals";
 import { totalCategory } from "./total-category";
 
 describe("totalCategory", () => {
-  it("should return total amount of each category", () => {
+  it("filterを指定してない場合、全量を集計する。", () => {
     const details = [
-      { id: "1", categoryId: "1", categoryName: "Food", amount: 100 },
-      { id: "2", categoryId: "2", categoryName: "Drink", amount: 200 },
-      { id: "3", categoryId: "1", categoryName: "Food", amount: 300 },
-      { id: "4", categoryId: "2", categoryName: "Drink", amount: 200 },
-      { id: "5", categoryId: "3", categoryName: "Transport", amount: 400 },
+      { type: "A", categoryId: "1", categoryName: "Food", amount: 100 },
+      { type: "A", categoryId: "2", categoryName: "Drink", amount: 200 },
+      { type: "B", categoryId: "1", categoryName: "Food", amount: 300 },
+      { type: "B", categoryId: "2", categoryName: "Drink", amount: 200 },
+      { type: "B", categoryId: "3", categoryName: "Transport", amount: 400 },
     ];
 
     const result = totalCategory({ details });
@@ -18,6 +18,26 @@ describe("totalCategory", () => {
       { categoryId: "1", categoryName: "Food", amount: 400 },
       { categoryId: "2", categoryName: "Drink", amount: 400 },
       { categoryId: "3", categoryName: "Transport", amount: 400 },
+    ]);
+  });
+
+  it("filterを指定すると、条件に合致するもののみ集計する。", () => {
+    const details = [
+      { type: "A", categoryId: "1", categoryName: "Food", amount: 100 },
+      { type: "A", categoryId: "2", categoryName: "Drink", amount: 200 },
+      { type: "B", categoryId: "1", categoryName: "Food", amount: 300 },
+      { type: "B", categoryId: "2", categoryName: "Drink", amount: 200 },
+      { type: "B", categoryId: "3", categoryName: "Transport", amount: 400 },
+    ];
+
+    const result = totalCategory({
+      details,
+      filter: (d) => d.type === "A",
+    });
+
+    expect(result).toEqual([
+      { categoryId: "1", categoryName: "Food", amount: 100 },
+      { categoryId: "2", categoryName: "Drink", amount: 200 },
     ]);
   });
 });
--- apps/expo/src/feature/household/dashboard/CategoryRankingBox/total-category.ts
+++ apps/expo/src/feature/household/dashboard/CategoryRankingBox/total-category.ts
@@ -7,10 +7,13 @@ type WithAmountType = { amount: number } & BaseType;
 
 export const totalCategory = <T extends WithAmountType>({
   details,
+  filter = () => true,
 }: {
   details: T[];
+  filter?: (d: T) => boolean;
 }): WithAmountType[] => {
   const categoryList = details
+    .filter(filter)
     .filter(
       (x, i, arr) => arr.findIndex((y) => y.categoryId === x.categoryId) === i,
     )
@@ -25,6 +28,7 @@ export const totalCategory = <T extends WithAmountType>({
   return categoryList.map((c) => {
     const total = details
       .filter((d) => d.categoryId === c.categoryId)
+      .filter(filter)
       .reduce((acc, cur) => acc + cur.amount, 0);
 
     return {
