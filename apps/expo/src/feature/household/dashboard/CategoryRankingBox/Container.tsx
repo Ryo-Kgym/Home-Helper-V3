49b2a9c4b7c9e5da31ba08e4076b56e66ad30685
Ryo-Kgym <mraytoh@icloud.com>
2024-01-25 20:32:12 +0900

    Home-Helper-V3-40 - カテゴリごとに集計した結果を表示するようにした。

--- apps/expo/src/feature/household/dashboard/CategoryRankingBox/Container.tsx
+++ apps/expo/src/feature/household/dashboard/CategoryRankingBox/Container.tsx
@@ -1,32 +1,21 @@
-import { useGetAggregatedCategoriesByDateQuery } from "@/turbo/graphql/household";
-
 import { Presenter } from "~/feature/household/dashboard/CategoryRankingBox/Presenter";
+import { useGetCategoryTotal } from "~/feature/household/dashboard/CategoryRankingBox/useGetCategoryTotal";
 import { getMonth } from "~/func/date/get-month";
-import { useSaveGroupId } from "~/hooks/group/useSaveGroupId";
-import { useGetTransferCategory } from "~/hooks/household/transfer/useGetTransferCategory";
 
 export const Container = () => {
-  const { groupId } = useSaveGroupId();
   const { firstDayOfMonth, lastDayOfMonth, month } = getMonth();
-
-  const { outcomeCategory } = useGetTransferCategory();
-
-  const [{ data }] = useGetAggregatedCategoriesByDateQuery({
-    variables: {
-      fromDate: firstDayOfMonth,
-      toDate: lastDayOfMonth,
-      groupId,
-      iocomeType: "OUTCOME",
-      limit: 10,
-      excludeCategoryIds: [outcomeCategory.id],
-    },
+  const { categoryTotal, loading } = useGetCategoryTotal({
+    fromDate: firstDayOfMonth,
+    toDate: lastDayOfMonth,
   });
 
-  const categories =
-    data?.group?.dailies.map((d) => ({
-      categoryName: d.categoryName ?? "",
-      amount: d.total as number,
-    })) ?? [];
-
-  return <Presenter month={month} categories={categories} />;
+  return (
+    <Presenter
+      month={month}
+      categories={categoryTotal
+        .sort((a, b) => b.amount - a.amount)
+        .slice(0, 10)}
+      loading={loading}
+    />
+  );
 };
--- apps/expo/src/feature/household/dashboard/CategoryRankingBox/Presenter.tsx
+++ apps/expo/src/feature/household/dashboard/CategoryRankingBox/Presenter.tsx
@@ -6,28 +6,34 @@ import { DashboardFrame } from "~/feature/household/dashboard/DashboardFrame";
 export const Presenter = ({
   month,
   categories,
+  loading,
 }: {
   month: number;
   categories: {
     categoryName: string;
     amount: number;
   }[];
+  loading: boolean;
 }) => (
   <DashboardFrame
-    label={`${month}月のランキング`}
+    label={`${month}月の支出ランキング`}
     href={paths.household.calendar(new Date())}
     size={"w-full"}
   >
-    <View>
-      {categories.map((c, i) => (
-        <View key={i} className={"flex-row items-center"}>
-          <Text className={"w-1/6 text-xl"}>{i + 1}</Text>
-          <Text className={"w-1/2 text-xl"}>{c.categoryName}</Text>
-          <Text className={"w-1/3 text-right text-2xl"}>
-            {c.amount.toLocaleString()}
-          </Text>
-        </View>
-      ))}
-    </View>
+    {loading ? (
+      <Text>Loading...</Text>
+    ) : (
+      <View>
+        {categories.map((c, i) => (
+          <View key={i} className={"flex-row items-center"}>
+            <Text className={"w-1/6 text-lg"}>{i + 1}</Text>
+            <Text className={"w-1/2 text-xl"}>{c.categoryName}</Text>
+            <Text className={"w-1/3 text-right text-2xl"}>
+              {c.amount.toLocaleString()}
+            </Text>
+          </View>
+        ))}
+      </View>
+    )}
   </DashboardFrame>
 );
