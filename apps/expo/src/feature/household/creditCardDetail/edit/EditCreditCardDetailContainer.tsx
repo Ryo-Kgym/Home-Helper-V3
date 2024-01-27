ade636a982243ca06fc9018899002b227900d004
Ryo-Kgym <mraytoh@icloud.com>
2024-01-27 21:43:02 +0900

    Home-Helper-V3-46 - クレジットカード明細編集への仮実装

---
+++ apps/expo/src/feature/household/creditCardDetail/edit/EditCreditCardDetailContainer.tsx
@@ -0,0 +1,135 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "expo-router";
+
+import type { IocomeType } from "~/types/iocome-type";
+import { useDeleteDaily } from "~/hooks/household/daily/useDeleteDaily";
+import { useEditDaily } from "~/hooks/household/daily/useEditDaily";
+import { useGetDailyById } from "~/hooks/household/daily/useGetDailyById";
+import { useAlert } from "~/hooks/useAlert";
+import { EditCreditCardDetailPresenter } from "./EditCreditCardDetailPresenter";
+
+export const EditCreditCardDetailContainer = ({ id }: { id: string }) => {
+  const { daily, loading } = useGetDailyById({ id });
+
+  const [date, setDate] = useState<Date | undefined>(daily.date);
+  const [iocomeType, setIocomeType] = useState<IocomeType>("INCOME");
+  const [genreId, setGenreId] = useState<string>("");
+  const [categoryId, setCategoryId] = useState<string>("");
+  const [accountId, setAccountId] = useState<string>("");
+  const [amount, setAmount] = useState<number | null>(null);
+  const [memo, setMemo] = useState<string | null>(null);
+
+  const { editDaily } = useEditDaily();
+  const { deleteDaily } = useDeleteDaily();
+  const { alertBuilder } = useAlert();
+  const { back } = useRouter();
+
+  const resetHandler = () => {
+    setDate(daily.date);
+    setIocomeType(daily.genre.iocomeType);
+    setGenreId(daily.genre.id);
+    setCategoryId(daily.category.id);
+    setAccountId(daily.account.id);
+    setAmount(daily.amount);
+    setMemo(daily.memo);
+  };
+
+  const editHandler = async () => {
+    if (!amount) return;
+    try {
+      await editDaily({
+        id,
+        date: date!,
+        iocomeType,
+        genreId,
+        categoryId,
+        accountId,
+        amount,
+        memo,
+      });
+      alert("更新しました");
+      back();
+    } catch (e) {
+      console.error(e);
+      alert("更新に失敗しました");
+    }
+  };
+
+  const deleteHandler = async () => {
+    try {
+      await deleteDaily(id);
+      alert("削除しました");
+      back();
+    } catch (e) {
+      console.error(e);
+      alert("削除に失敗しました");
+    }
+  };
+
+  const onPressAlert = () => {
+    alertBuilder({
+      title: "削除しますか？",
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      okCallback: deleteHandler,
+    });
+  };
+
+  useEffect(
+    () => {
+      setDate(daily.date);
+      setIocomeType(daily.genre.iocomeType);
+      setGenreId(daily.genre.id);
+      setCategoryId(daily.category.id);
+      setAccountId(daily.account.id);
+      setAmount(daily.amount);
+      setMemo(daily.memo);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [loading],
+  );
+
+  return (
+    <EditCreditCardDetailPresenter
+      id={id}
+      date={{
+        value: date,
+        default: daily.date,
+        setValue: setDate,
+      }}
+      iocomeType={{
+        value: iocomeType,
+        default: daily.genre.iocomeType,
+        setValue: setIocomeType,
+      }}
+      genre={{
+        value: genreId,
+        default: daily.genre.id,
+        setValue: setGenreId,
+      }}
+      category={{
+        value: categoryId,
+        default: daily.category.id,
+        setValue: setCategoryId,
+      }}
+      account={{
+        value: accountId,
+        default: daily.account.id,
+        setValue: setAccountId,
+      }}
+      amount={{
+        value: amount,
+        default: daily.amount,
+        setValue: setAmount,
+      }}
+      memo={{
+        value: memo,
+        default: daily.memo,
+        setValue: setMemo,
+      }}
+      resetHandler={resetHandler}
+      editHandler={editHandler}
+      deleteHandler={onPressAlert}
+      disabled={loading}
+    />
+  );
+};
---
+++ apps/expo/src/feature/household/creditCardDetail/edit/EditCreditCardDetailPresenter.tsx
@@ -0,0 +1,140 @@
+import {
+  Button,
+  Keyboard,
+  KeyboardAvoidingView,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+
+import type { IocomeType } from "~/types/iocome-type";
+import {
+  EditableAccount,
+  EditableAmount,
+  EditableCategory,
+  EditableDate,
+  EditableGenre,
+  EditableIocomeType,
+  EditableMemo,
+} from "~/ui";
+
+type FieldType<T> = {
+  value: T;
+  setValue: (value: T) => void;
+  default: T;
+};
+
+export const EditCreditCardDetailPresenter = ({
+  date,
+  iocomeType,
+  genre,
+  category,
+  amount,
+  account,
+  memo,
+  resetHandler,
+  editHandler,
+  deleteHandler,
+  disabled,
+}: {
+  id: string;
+  date: FieldType<Date | undefined>;
+  iocomeType: FieldType<IocomeType>;
+  genre: FieldType<string>;
+  category: FieldType<string>;
+  account: FieldType<string>;
+  amount: FieldType<number | null>;
+  memo: FieldType<string | null>;
+  resetHandler: () => void;
+  editHandler: () => void;
+  deleteHandler: () => void;
+  disabled: boolean;
+}) => (
+  <KeyboardAvoidingView
+    behavior={"position"}
+    contentContainerStyle={{}}
+    keyboardVerticalOffset={100}
+  >
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View className={"gap-3"}>
+        <View>
+          <Text>日付</Text>
+          <EditableDate
+            value={date.value}
+            setValue={date.setValue}
+            loadingValue={"-"}
+          />
+        </View>
+        <View>
+          <Text>タイプ</Text>
+          <EditableIocomeType
+            value={iocomeType.value}
+            setValue={iocomeType.setValue}
+            defaultValue={iocomeType.default}
+          />
+        </View>
+        <View>
+          <Text>ジャンル</Text>
+          <EditableGenre
+            value={genre.value}
+            setValue={genre.setValue}
+            defaultValue={genre.default}
+            iocomeType={iocomeType.value}
+          />
+        </View>
+        <View>
+          <Text>カテゴリ</Text>
+          <EditableCategory
+            value={category.value}
+            setValue={category.setValue}
+            defaultValue={category.default}
+            genreId={genre.value}
+          />
+        </View>
+        <View>
+          <Text>アカウント</Text>
+          <EditableAccount
+            value={account.value}
+            setValue={account.setValue}
+            defaultValue={account.default}
+            disabled
+          />
+        </View>
+        <View>
+          <Text>金額</Text>
+          <EditableAmount
+            value={amount.value}
+            setValue={amount.setValue}
+            iocomeType={iocomeType.value}
+            defaultValue={amount.default}
+            disabled
+          />
+        </View>
+        <View>
+          <Text>メモ</Text>
+          <EditableMemo
+            value={memo.value}
+            setValue={memo.setValue}
+            defaultValue={memo.default ?? undefined}
+          />
+        </View>
+        <View className={"flex-row justify-between"}>
+          <View className={"w-1/3"}>
+            <Button title={"更新"} onPress={editHandler} disabled={disabled} />
+          </View>
+          <View className={"w-1/3"}>
+            <Button
+              title={"削除"}
+              onPress={deleteHandler}
+              disabled={disabled}
+              color={"#FF0000"}
+            />
+          </View>
+          <View className={"w-1/3"}>
+            <Button title={"リセット"} onPress={resetHandler} />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  </KeyboardAvoidingView>
+);
