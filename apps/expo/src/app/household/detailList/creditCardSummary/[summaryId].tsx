34284ec833818c5c1374696354da60f5f3def289
Ryo-Kgym <mraytoh@icloud.com>
2024-01-29 10:58:45 +0900

    Home-Helper-V3-51 - タイトルを追加した。

--- apps/expo/src/app/household/detailList/creditCardSummary/[summaryId].tsx
+++ apps/expo/src/app/household/detailList/creditCardSummary/[summaryId].tsx
@@ -2,12 +2,21 @@ import { View } from "react-native";
 import { Stack, useLocalSearchParams } from "expo-router";
 
 import { DetailListByCreditCardSummary } from "~/feature/household/detailList";
+import { useGetCreditCardSummaryById } from "~/hooks/household/credit_card/useGetCreditCardSummaryById";
 
 const Page = () => {
   const { summaryId } = useLocalSearchParams();
+  const { creditCardSummary } = useGetCreditCardSummaryById({
+    summaryId: summaryId as string,
+  });
+
   return (
     <View>
-      <Stack.Screen options={{ title: "" }} />
+      <Stack.Screen
+        options={{
+          title: creditCardSummary.genre.name + "の引き落とし",
+        }}
+      />
       <View className={"w-full"}>
         <DetailListByCreditCardSummary summaryId={summaryId as string} />
       </View>
