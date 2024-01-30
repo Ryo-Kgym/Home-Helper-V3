ba4f0220503f9141b92245640c46fec0f20134ab
Ryo-Kgym <mraytoh@icloud.com>
2024-01-30 09:26:12 +0900

    Home-Helper-V3-55 - 一覧を作成した。

--- apps/expo/src/feature/household/rank/category/yearly/YearlyCategoryRankList.tsx
+++ apps/expo/src/feature/household/rank/category/yearly/YearlyCategoryRankList.tsx
@@ -1,9 +1,49 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { FlatList, Text, View } from "react-native";
 
-export const YearlyCategoryRankList = (_: { year: Date }) => {
+import type { IocomeType } from "~/types/iocome-type";
+import { getYear } from "~/func/date/get-year";
+import { useGetCategoryTotal } from "~/hooks/household/total/useGetCategoryTotal";
+import { EditableIocomeType } from "~/ui";
+
+export const YearlyCategoryRankList = ({ year }: { year: Date }) => {
+  const [iocomeType, setIocomeType] = useState<IocomeType>("INCOME");
+
+  const { firstDayOfYear, lastDateNotGreaterThanToday } = getYear(year);
+  const { categoryTotal, loading } = useGetCategoryTotal({
+    fromDate: firstDayOfYear,
+    toDate: lastDateNotGreaterThanToday,
+    iocomeType: [iocomeType],
+  });
+
+  if (loading) return <Text>loading...</Text>;
+
   return (
-    <View>
-      <Text>AAA</Text>
-    </View>
+    <>
+      {/*TODO モーダルに隠す*/}
+      <EditableIocomeType value={iocomeType} setValue={setIocomeType} />
+      <FlatList
+        data={categoryTotal}
+        keyExtractor={(item) => item.categoryId}
+        renderItem={({ item, index }) => (
+          <View
+            key={item.categoryId}
+            className={"flex-row items-center border-b border-b-gray-300 py-2"}
+          >
+            <Text className={"w-1/12 pr-2 text-right text-sm text-gray-600"}>
+              {index + 1}
+            </Text>
+            <View className={"flex-1 flex-row justify-between"}>
+              <Text className={"text-xl text-gray-600"}>
+                {item.categoryName}
+              </Text>
+              <Text className={"pr-2 text-right text-xl"}>
+                {item.amount.toLocaleString()}
+              </Text>
+            </View>
+          </View>
+        )}
+      />
+    </>
   );
 };
