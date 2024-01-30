0b1edd3ef60345050eb138099316c42e7f51bfc5
Ryo-Kgym <mraytoh@icloud.com>
2024-01-30 10:22:15 +0900

    Home-Helper-V3-55 - sort関数を作成した。

---
+++ apps/expo/src/hooks/household/total/sort-by.ts
@@ -0,0 +1,18 @@
+import type { WithAmountType } from "./total-category";
+
+type sortKey = "amount";
+
+export const sortBy: Record<
+  sortKey,
+  {
+    [orientation in "asc" | "desc"]: (
+      a: WithAmountType,
+      b: WithAmountType,
+    ) => number;
+  }
+> = {
+  amount: {
+    asc: (a, b) => (a.amount > b.amount ? 1 : -1),
+    desc: (a, b) => (a.amount < b.amount ? 1 : -1),
+  },
+};
