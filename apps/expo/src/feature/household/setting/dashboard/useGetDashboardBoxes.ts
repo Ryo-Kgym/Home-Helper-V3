2f0428b865ad2fc1f497170a572f9fda753a80b8
Ryo-Kgym <mraytoh@icloud.com>
2024-02-01 12:18:00 +0900

    Home-Helper-V3-64 - BOXを取得するhookを作成した。

---
+++ apps/expo/src/feature/household/setting/dashboard/useGetDashboardBoxes.ts
@@ -0,0 +1,57 @@
+import type { BoxesType } from "~/feature/household/setting/dashboard/type";
+import { generateBox } from "~/feature/household/setting/dashboard/generate-box";
+
+export const useGetDashboardBoxes = () => {
+  const getDashboardBoxes = (): BoxesType => {
+    return [
+      {
+        feature: "balance",
+        argsMap: [],
+      },
+      {
+        feature: "register",
+        argsMap: [],
+      },
+      {
+        feature: "setting",
+        argsMap: [],
+      },
+      {
+        feature: "yearly",
+        argsMap: [{ type: "year", value: -2 }],
+      },
+      {
+        feature: "yearly",
+        argsMap: [{ type: "year", value: -1 }],
+      },
+      {
+        feature: "yearly",
+        argsMap: [{ type: "year", value: 0 }],
+      },
+      {
+        feature: "monthly",
+        argsMap: [{ type: "month", value: -2 }],
+      },
+      {
+        feature: "monthly",
+        argsMap: [{ type: "month", value: -1 }],
+      },
+      {
+        feature: "monthly",
+        argsMap: [{ type: "month", value: 0 }],
+      },
+      {
+        feature: "monthlyCategory",
+        argsMap: [{ type: "month", value: -1 }],
+      },
+      {
+        feature: "monthlyCategory",
+        argsMap: [{ type: "month", value: 0 }],
+      },
+    ];
+  };
+
+  const getBoxNodes = (): React.ReactNode[] => generateBox(getDashboardBoxes());
+
+  return { getBoxNodes };
+};
