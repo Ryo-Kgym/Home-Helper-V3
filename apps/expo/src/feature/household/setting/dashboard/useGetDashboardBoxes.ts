9fb6bfa703abd700f80c5e451e6a70a2ed274291
Ryo-Kgym <mraytoh@icloud.com>
2024-02-05 00:36:40 +0900

    Home-Helper-V3-64 - 設定値をDBから取得するようにした。

--- apps/expo/src/feature/household/setting/dashboard/useGetDashboardBoxes.ts
+++ apps/expo/src/feature/household/setting/dashboard/useGetDashboardBoxes.ts
@@ -1,57 +1,43 @@
-import type { BoxesType } from "./type";
+import { useGetDashboardSettingQuery } from "@v3/graphql/household";
+
+import type { ArgsMapType, ArgsType, BoxesType, Feature } from "./type";
+import { useSaveGroupId } from "~/hooks/group/useSaveGroupId";
+import { useSaveUserId } from "~/hooks/user/useSaveUserId";
 import { generateBox } from "./generate-box";
 
 export const useGetDashboardBoxes = () => {
-  const getDashboardBoxes = (): BoxesType => {
-    return [
-      {
-        feature: "balance",
-        argsMap: [],
-      },
-      {
-        feature: "register",
-        argsMap: [],
-      },
-      // {
-      //   feature: "setting",
-      //   argsMap: [],
-      // },
-      {
-        feature: "yearly",
-        argsMap: [{ type: "year", value: -2 }],
-      },
-      {
-        feature: "yearly",
-        argsMap: [{ type: "year", value: -1 }],
-      },
-      {
-        feature: "yearly",
-        argsMap: [{ type: "year", value: 0 }],
-      },
-      {
-        feature: "monthly",
-        argsMap: [{ type: "month", value: -2 }],
-      },
-      {
-        feature: "monthly",
-        argsMap: [{ type: "month", value: -1 }],
-      },
-      {
-        feature: "monthly",
-        argsMap: [{ type: "month", value: 0 }],
-      },
-      {
-        feature: "monthlyCategory",
-        argsMap: [{ type: "month", value: -1 }],
-      },
-      {
-        feature: "monthlyCategory",
-        argsMap: [{ type: "month", value: 0 }],
-      },
-    ];
-  };
+  const { userId } = useSaveUserId();
+  const { groupId } = useSaveGroupId();
 
-  const getBoxNodes = (): React.ReactNode[] => generateBox(getDashboardBoxes());
+  const [{ data }] = useGetDashboardSettingQuery({
+    variables: {
+      userId,
+      groupId,
+    },
+  });
+
+  const setting: BoxesType =
+    data?.setting.map((s) => {
+      return {
+        feature: s.feature as Feature,
+        argsMap: s.args.map((a) => valueConverter[a.type as ArgsType](a.value)),
+      };
+    }) ?? [];
+
+  const getBoxNodes = (): React.ReactNode[] => generateBox(setting);
 
   return { getBoxNodes };
+};
+
+const valueConverter: {
+  [type in ArgsType]: (value: string) => ArgsMapType;
+} = {
+  year: (value: string) => ({
+    type: "year",
+    value: Number(value),
+  }),
+  month: (value: string) => ({
+    type: "month",
+    value: Number(value),
+  }),
 };
