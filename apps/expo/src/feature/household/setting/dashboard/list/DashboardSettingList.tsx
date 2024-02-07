17c9efe1e4355cd4adaee81a96447051e8cbc943
Ryo-Kgym <mraytoh@icloud.com>
2024-02-07 21:20:16 +0900

    Home-Helper-V3-66 - 循環参照を解消した。

--- apps/expo/src/feature/household/setting/dashboard/list/DashboardSettingList.tsx
+++ apps/expo/src/feature/household/setting/dashboard/list/DashboardSettingList.tsx
@@ -2,68 +2,67 @@ import { FlatList, Pressable, Text, View } from "react-native";
 import { Link } from "expo-router";
 
 import { paths } from "~/app/paths";
-import { useGetDashboardBoxes } from "~/feature/household/setting";
+import { useGetDashboardBoxes } from "../useGetDashboardBoxes";
 import { featureMap } from "./feature-map";
 
 export const DashboardSettingList = () => {
   const { setting } = useGetDashboardBoxes();
 
   return (
-    <View>
-      <FlatList
-        data={setting}
-        renderItem={({ item, index }) => (
-          <Pressable
-            style={{
-              borderStyle: "solid",
-              borderBottomWidth: 0.5,
-              borderColor: "gray",
-            }}
-          >
-            <Link href={paths.household.setting.dashboardEdit({ id: item.id })}>
+    <FlatList
+      data={setting}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item, index }) => (
+        <Pressable
+          style={{
+            borderStyle: "solid",
+            borderBottomWidth: 0.5,
+            borderColor: "gray",
+          }}
+        >
+          <Link href={paths.household.setting.dashboardEdit({ id: item.id })}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                paddingTop: 10,
+                paddingBottom: 8,
+              }}
+            >
+              <Text
+                style={{
+                  width: 20,
+                  textAlign: "right",
+                }}
+              >
+                {index + 1}
+              </Text>
               <View
                 style={{
                   flexDirection: "row",
+                  justifyContent: "space-between",
                   alignItems: "center",
-                  paddingTop: 10,
-                  paddingBottom: 8,
+                  flex: 1,
+                  paddingHorizontal: 10,
                 }}
               >
+                <Text className={"text-xl"}>
+                  {featureMap[item.feature].label}
+                </Text>
                 <Text
                   style={{
-                    width: 20,
                     textAlign: "right",
                   }}
                 >
-                  {index + 1}
+                  {item.argsMap
+                    .map((at) => at.type + "= " + at.value)
+                    .join(", ")}
                 </Text>
-                <View
-                  style={{
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                    flex: 1,
-                    paddingHorizontal: 10,
-                  }}
-                >
-                  <Text className={"text-xl"}>
-                    {featureMap[item.feature].label}
-                  </Text>
-                  <Text
-                    style={{
-                      textAlign: "right",
-                    }}
-                  >
-                    {item.argsMap
-                      .map((at) => at.type + "= " + at.value)
-                      .join(", ")}
-                  </Text>
-                </View>
               </View>
-            </Link>
-          </Pressable>
-        )}
-      />
-    </View>
+            </View>
+          </Link>
+        </Pressable>
+      )}
+    />
   );
 };
