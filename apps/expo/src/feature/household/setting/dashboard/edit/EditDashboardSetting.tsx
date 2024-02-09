84bc790e4e73c6c5cae4864968bde140935b6ad4
Ryo-Kgym <mraytoh@icloud.com>
2024-02-09 22:29:37 +0900

    Home-Helper-V3-66 - 更新、リセットボタンを追加した。

--- apps/expo/src/feature/household/setting/dashboard/edit/EditDashboardSetting.tsx
+++ apps/expo/src/feature/household/setting/dashboard/edit/EditDashboardSetting.tsx
@@ -1,16 +1,11 @@
 import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
-import type {
-  ArgsMapType,
-  Feature,
-} from "~/feature/household/setting/dashboard/type";
-import {
-  generateMonthOptions,
-  generateYearOptions,
-} from "~/feature/household/setting/dashboard/edit/args-value-range";
-import { featureMap } from "~/feature/household/setting/dashboard/list/feature-map";
+import type { ArgsMapType, Feature } from "../type";
+import { ResetButton, UpdateButton } from "~/ui";
 import { Picker } from "~/ui/Picker";
+import { featureMap } from "../list/feature-map";
+import { generateMonthOptions, generateYearOptions } from "./args-value-range";
 
 export const EditDashboardSetting = ({
   detail,
@@ -24,59 +19,85 @@ export const EditDashboardSetting = ({
     | undefined;
 }) => {
   const [feature, setFeature] = useState<Feature | null>(null);
-  const [args, setArgs] = useState<ArgsMapType[]>([]);
+  const [argsMapTypes, setArgsMapTypes] = useState<ArgsMapType[]>([]);
 
   const featurePickerData = Object.keys(featureMap).map((f) => ({
     label: featureMap[f as Feature].label,
     value: f as Feature,
   }));
 
+  const updateHandler = () => {
+    console.error("feature", feature, "args", argsMapTypes);
+  };
+
+  const resetHandler = () => {
+    if (detail) {
+      setFeature(detail.feature);
+      setArgsMapTypes(detail.argsMap);
+    }
+  };
+
   useEffect(() => {
     if (detail) {
       setFeature(detail.feature);
-      setArgs(detail.argsMap);
+      setArgsMapTypes(detail.argsMap);
     }
   }, [detail]);
 
-  if (!detail) return null;
+  if (!detail || !feature) return null;
 
   return (
     <View>
       <Picker value={feature} setValue={setFeature} data={featurePickerData} />
       <View>
-        {feature &&
-          featureMap[feature]?.argsTypes.map((argsType, index) => {
-            if (!args[index]) return null;
+        {featureMap[feature].argsTypes.map((type, index) => {
+          if (!argsMapTypes[index]) return null;
 
-            return (
-              <View key={argsType}>
-                <Text>{argsType}</Text>
-                {/* TODO 種類が増えたらリファクタリングする*/}
-                {argsType === "year" && (
-                  <Picker
-                    value={args[index]!.value}
-                    setValue={(v) => {
-                      const newArgs = [...args];
-                      newArgs[index] = { type: argsType, value: v };
-                      setArgs(newArgs);
-                    }}
-                    data={generateYearOptions()}
-                  />
-                )}
-                {argsType === "month" && (
-                  <Picker
-                    value={args[index]!.value}
-                    setValue={(v) => {
-                      const newArgs = [...args];
-                      newArgs[index] = { type: argsType, value: v };
-                      setArgs(newArgs);
-                    }}
-                    data={generateMonthOptions()}
-                  />
-                )}
-              </View>
-            );
-          })}
+          return (
+            <View key={type}>
+              <Text>{type}</Text>
+              {/* TODO 種類が増えたらリファクタリングする*/}
+              {type === "year" && (
+                <Picker
+                  value={argsMapTypes[index]!.value}
+                  setValue={(value) => {
+                    const newArgs = [...argsMapTypes];
+                    newArgs[index] = { type, value };
+                    setArgsMapTypes(newArgs);
+                  }}
+                  data={generateYearOptions()}
+                />
+              )}
+              {type === "month" && (
+                <Picker
+                  value={argsMapTypes[index]!.value}
+                  setValue={(value) => {
+                    const newArgs = [...argsMapTypes];
+                    newArgs[index] = { type, value };
+                    setArgsMapTypes(newArgs);
+                  }}
+                  data={generateMonthOptions()}
+                />
+              )}
+            </View>
+          );
+        })}
+      </View>
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <View
+          style={{
+            width: "50%",
+          }}
+        >
+          <UpdateButton updateHandler={updateHandler} />
+        </View>
+        <View
+          style={{
+            width: "50%",
+          }}
+        >
+          <ResetButton resetHandler={resetHandler} />
+        </View>
       </View>
     </View>
   );
