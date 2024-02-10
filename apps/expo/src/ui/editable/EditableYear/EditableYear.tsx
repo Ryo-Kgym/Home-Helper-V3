70cf19a53c40fb45d185f0f9fdaea8e4d208562d
Ryo-Kgym <mraytoh@icloud.com>
2024-02-10 14:08:47 +0900

    Home-Helper-V3-70 - EditableYear 作成完了

--- apps/expo/src/ui/editable/EditableYear/EditableYear.tsx
+++ apps/expo/src/ui/editable/EditableYear/EditableYear.tsx
@@ -1,9 +1,18 @@
-import type { EditableProps } from "~/ui/editable/editable-props";
+import type { EditableProps } from "../editable-props";
 import { Picker } from "~/ui/Picker";
+import { generateYearOptions } from "./generate-year-options";
 
 export const EditableYear = ({
   value,
   setValue,
+  range,
 }: EditableProps<number> & { range?: { min: number; max: number } }) => {
-  return <Picker value={value} setValue={setValue} data={[]} />;
+  const { min, max } = range ?? { min: -10, max: 0 };
+  return (
+    <Picker
+      value={value}
+      setValue={setValue}
+      data={generateYearOptions({ min, max })}
+    />
+  );
 };
