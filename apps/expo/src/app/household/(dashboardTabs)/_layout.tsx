7d550481149034db09c01fd4214a4235b68531ef
Ryo-Kgym <mraytoh@icloud.com>
2024-02-12 00:10:26 +0900

    Home-Helper-V3-72 - ダッシュボード周りのレイアウトを変更した。

---
+++ apps/expo/src/app/household/(dashboardTabs)/_layout.tsx
@@ -0,0 +1,38 @@
+import { Stack, Tabs } from "expo-router";
+import { AntDesign, Entypo, Octicons } from "@expo/vector-icons";
+
+const Layout = () => (
+  <>
+    <Stack.Screen options={{ title: "" }} />
+    <Tabs>
+      <Tabs.Screen
+        name="dashboard/index"
+        options={{
+          title: "ダッシュボード",
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="dashboard" size={30} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="register/index"
+        options={{
+          title: "登録",
+          tabBarIcon: ({ color }) => (
+            <Entypo name="pencil" size={30} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="setting/index"
+        options={{
+          title: "設定",
+          tabBarIcon: ({ color }) => (
+            <Octicons name="gear" size={30} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  </>
+);
+export default Layout;
