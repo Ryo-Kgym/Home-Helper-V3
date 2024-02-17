8fdbc34d71c793cc442b6718f098608cd8aab763
Ryo-Kgym <mraytoh@icloud.com>
2024-02-17 23:07:29 +0900

    Home-Helper-V3-88 - お気に入り条件一覧を作成した。

---
+++ apps/expo/src/feature/household/setting/favoriteFilter/list/FavoriteFilterList.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export const FavoriteFilterList = () => {
+  return (
+    <View>
+      <Text>サンプル</Text>
+    </View>
+  );
+};
--- apps/expo/src/feature/household/setting/index.ts
+++ apps/expo/src/feature/household/setting/index.ts
@@ -1,2 +1,3 @@
 export { DashboardSettingList as DashboardSettingList } from "./dashboard/list/DashboardSettingList";
+export { FavoriteFilterList as FavoriteFilterList } from "./favoriteFilter/list/FavoriteFilterList";
 export { Setting as Setting } from "./Setting";
