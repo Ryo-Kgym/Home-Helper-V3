76b543916fcf49f3367cabdb964e019a195045e3
Ryo-Kgym <mraytoh@icloud.com>
2024-02-17 23:46:02 +0900

    Home-Helper-V3-88 - お気に入り条件の一覧を表示した。

--- apps/expo/src/feature/household/setting/favoriteFilter/list/FavoriteFilterList.tsx
+++ apps/expo/src/feature/household/setting/favoriteFilter/list/FavoriteFilterList.tsx
@@ -1,9 +1,41 @@
-import { Text, View } from "react-native";
+import { FlatList, Pressable, Text, View } from "react-native";
+import { useGetFavoriteFiltersQuery } from "@v3/graphql/household";
+
+import { useSaveGroupId } from "~/hooks/group/useSaveGroupId";
 
 export const FavoriteFilterList = () => {
+  const { groupId } = useSaveGroupId();
+
+  const [{ data }] = useGetFavoriteFiltersQuery({ variables: { groupId } });
+
+  const favoriteFilters =
+    data?.filters.map((f) => ({
+      id: f.id,
+      name: f.name,
+    })) ?? [];
+
   return (
     <View>
-      <Text>サンプル</Text>
+      <FlatList
+        data={favoriteFilters}
+        keyExtractor={({ id }) => id as string}
+        renderItem={({ item }) => (
+          <Pressable
+            style={{
+              borderStyle: "solid",
+              borderBottomWidth: 0.5,
+              borderColor: "gray",
+              paddingTop: 10,
+              paddingBottom: 8,
+              paddingLeft: 10,
+            }}
+          >
+            <View>
+              <Text className={"text-xl"}>{item.name}</Text>
+            </View>
+          </Pressable>
+        )}
+      />
     </View>
   );
 };
