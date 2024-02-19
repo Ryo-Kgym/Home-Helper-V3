32629b38e16229579666295549b6b5db794f3347
Ryo-Kgym <mraytoh@icloud.com>
2024-02-19 12:37:33 +0900

    Home-Helper-V3-88 - 年のリストを生成するようにした。

--- apps/expo/src/feature/household/setting/favoriteFilter/ui/_input/YearInput.tsx
+++ apps/expo/src/feature/household/setting/favoriteFilter/ui/_input/YearInput.tsx
@@ -4,13 +4,17 @@ import { Picker } from "~/ui/Picker";
 export const YearInput: FavoriteFilterArgValueInputComponent = ({
   value,
   setValue,
-}) => (
-  <Picker
-    value={value}
-    setValue={setValue}
-    data={[
-      { value: "2021", label: "2021" },
-      { value: "2022", label: "2022" },
-    ]}
-  />
-);
+}) => <Picker value={value} setValue={setValue} data={generateYearData()} />;
+
+const generateYearData = () => {
+  const range = { min: -5, max: 0 };
+  const now = new Date();
+  const year = now.getFullYear();
+  const min = year + range.min;
+  const max = year + range.max;
+  const years = [];
+  for (let i = min; i <= max; i++) {
+    years.push({ value: i.toString(), label: i.toString() });
+  }
+  return years;
+};
