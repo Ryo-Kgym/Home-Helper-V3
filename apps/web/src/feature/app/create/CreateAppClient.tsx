538ca23a8da810c914480407e94564556ff285dc
Ryo-Kgym <mraytoh@icloud.com>
2024-03-06 10:27:05 +0900

    One-for-All-104 - Create the FieldAddButton.

--- apps/web/src/feature/app/create/CreateAppClient.tsx
+++ apps/web/src/feature/app/create/CreateAppClient.tsx
@@ -4,6 +4,7 @@ import type { FieldKind } from "@oneforall/domain/field/type";
 import { useEffect, useState } from "react";
 import { AppAddField } from "@feature/app/create/AppAddField";
 import { AppNameInput } from "@feature/app/create/AppNameInput";
+import { FieldAddButton } from "@feature/app/create/FieldAddButton";
 
 export const CreateAppClient = () => {
   const [appName, setAppName] = useState<string>("");
@@ -15,7 +16,10 @@ export const CreateAppClient = () => {
 
   return (
     <>
-      <AppNameInput appName={appName} setAppName={setAppName} />
+      <div className={"grid grid-cols-2 space-x-2"}>
+        <AppNameInput appName={appName} setAppName={setAppName} />
+        <FieldAddButton />
+      </div>
       <AppAddField index={0} value={value} setValue={setValue} />
       <AppAddField index={1} value={value} setValue={setValue} />
       <AppAddField index={2} value={value} setValue={setValue} />
---
+++ apps/web/src/feature/app/create/FieldAddButton.tsx
@@ -0,0 +1,7 @@
+export const FieldAddButton = () => {
+  return (
+    <button className={"p-2 hover:bg-blue-100 active:bg-blue-300"}>
+      フィールド追加
+    </button>
+  );
+};
