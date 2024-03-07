7c0b469ac8449862385613b5ab8f87dbd50b9d67
Ryo-Kgym <mraytoh@icloud.com>
2024-03-08 00:40:55 +0900

    One-for-All-106 - I made the app's items display in a table format.

--- apps/web/src/feature/app/show/ShowAppClient.tsx
+++ apps/web/src/feature/app/show/ShowAppClient.tsx
@@ -3,12 +3,21 @@ import type { App } from "@feature/app/schema/app-schema";
 export const ShowAppClient = ({ app }: { app: App }) => {
   return (
     <div>
-      <h1>{app.name}</h1>
-      <ul>
-        {Object.values(app.fields).map((field) => {
-          return <li key={field.fieldName}>{field.fieldName}</li>;
-        })}
-      </ul>
+      <div className={"text-3xl"}>{app.name}</div>
+      <table>
+        <thead>
+          <tr className={"bg-gray-50"}>
+            {Object.values(app.fields).map((field) => (
+              <td
+                key={field.fieldName}
+                className={"border border-gray-300 p-2"}
+              >
+                {field.fieldName}
+              </td>
+            ))}
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 };
