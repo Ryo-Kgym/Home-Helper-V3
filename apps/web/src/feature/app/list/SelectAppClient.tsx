b7db0715af7739028460caad27b35d46e19e25ac
Ryo-Kgym <mraytoh@icloud.com>
2024-03-07 17:13:46 +0900

    One-for-All-104 - Close

--- apps/web/src/feature/app/list/SelectAppClient.tsx
+++ apps/web/src/feature/app/list/SelectAppClient.tsx
@@ -11,7 +11,7 @@ import { LinkList } from "@components/atoms/Card";
 export const SelectAppClient = ({ data }: { data: GetApplicationsQuery }) => {
   const linkListProps: LinkProps[] = [
     { href: "/group", label: "戻る", back: true } as LinkProps,
-    { href: "/app/create", label: "Create App" } as LinkProps,
+    // { href: "/app/create", label: "Create App" } as LinkProps,
   ].concat(
     data?.group.map(({ app }) => ({
       href: app.topUrl,
