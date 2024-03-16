5ed7aacb6c836293bf6f4a530e6af614c12bee76
Ryo-Kgym <mraytoh@icloud.com>
2024-03-16 11:58:39 +0900

    One-for-All-115 - Changed the label.

--- apps/web/src/feature/app/modify/RedirectShowButton.tsx
+++
@@ -1,6 +0,0 @@
-import Link from "next/link";
-import { paths } from "@routing/paths";
-
-export const RedirectShowButton = ({ appId }: { appId: string }) => {
-  return <Link href={paths.app.show({ id: appId })}>アプリ</Link>;
-};
