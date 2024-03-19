ed1c2f6805c3cf2607b8f8f0acc0f7a84f8a295f
Ryo-Kgym <mraytoh@icloud.com>
2024-03-20 01:18:52 +0900

    One-for-All-115 - I implemented the table components.

---
+++ apps/web/src/components/ui/v4/table/TailWindCSSTable.tsx
@@ -0,0 +1,52 @@
+import type { ReactNode } from "react";
+
+export const TailWindCSSTable = ({ children }: { children: ReactNode }) => (
+  <table width={"100%"}>{children}</table>
+);
+
+const Header = ({ headerItems }: { headerItems: { name: string }[] }) => (
+  <thead>
+    <tr>
+      {headerItems.map(({ name }, index) => (
+        <th
+          key={`th-td-${index}}`}
+          className={"border border-gray-300 p-2 font-bold"}
+        >
+          {name}
+        </th>
+      ))}
+    </tr>
+  </thead>
+);
+
+const Body = <T,>({
+  data,
+  renderItem,
+}: {
+  data: T[];
+  renderItem: (t: T) => ReactNode;
+}) => (
+  <tbody>
+    {data.map((item, index) => (
+      <tr className={"border border-gray-300"} key={index}>
+        {renderItem(item)}
+      </tr>
+    ))}
+  </tbody>
+);
+
+const Td = ({
+  children,
+  align = "left",
+}: {
+  children: ReactNode;
+  align?: "left" | "center" | "right";
+}) => (
+  <td className={"border border-gray-300 p-2"} style={{ textAlign: align }}>
+    {children}
+  </td>
+);
+
+TailWindCSSTable.Header = Header;
+TailWindCSSTable.Body = Body;
+TailWindCSSTable.BodyTd = Td;
---
+++ apps/web/src/components/ui/v4/table/index.ts
@@ -0,0 +1,1 @@
+export { TailWindCSSTable as Table } from "./TailWindCSSTable";
