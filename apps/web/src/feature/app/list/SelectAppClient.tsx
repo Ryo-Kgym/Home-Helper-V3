80dae6147e674036d86d89b096a9dcfdb3b138d7
Ryo-Kgym <mraytoh@icloud.com>
2024-03-23 03:45:11 +0900

    One-for-All-117 - I enabled modal display for the list of apps.

--- apps/web/src/feature/app/list/SelectAppClient.tsx
+++ apps/web/src/feature/app/list/SelectAppClient.tsx
@@ -6,59 +6,74 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
+import { Modal } from "@components/ui/v4/modal";
 import { paths } from "@routing/paths";
 
 import type { AppListType } from "./SelectAppServer";
 
 export const SelectAppClient = ({ data }: { data: AppListType[] }) => {
   const { push } = useRouter();
+  const [isOpen, setIsOpen] = React.useState(false);
 
   return (
-    <div>
-      <div className={"grid grid-cols-3"}>
-        {[
-          {
-            label: "グループ選択へ",
-            href: paths.group.select,
-            back: true,
-          },
-          {
-            label: "アプリ新規作成",
-            href: paths.app.create,
-          },
-          {
-            label: "アプリ一覧",
-            href: "#",
-          },
-        ].map((app) => (
-          <LinkBox key={app.label} app={app} onClick={() => push(app.href)} />
-        ))}
-      </div>
+    <>
       <div>
-        {data.map((app) => (
-          <LinkBox key={app.label} app={app} onClick={() => push(app.href)} />
-        ))}
+        <div className={"grid grid-cols-3"}>
+          {[
+            {
+              label: "グループ選択へ",
+              back: true,
+              onClick: () => push(paths.group.select),
+            },
+            {
+              label: "アプリ新規作成",
+              onClick: () => push(paths.app.create),
+            },
+            {
+              label: "アプリ一覧",
+              onClick: () => setIsOpen(true),
+            },
+          ].map((props) => (
+            <LinkBox key={props.label} {...props} />
+          ))}
+        </div>
       </div>
-    </div>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <Modal.Body>
+          <div>アプリ一覧</div>
+          <div>
+            {data.map((props) => (
+              <LinkBox
+                key={props.label}
+                {...props}
+                onClick={() => push(props.href)}
+              />
+            ))}
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 };
 
 const LinkBox = ({
-  app,
+  label,
+  back,
   onClick,
 }: {
-  app: AppListType;
+  label: string;
+  back?: boolean;
   onClick: () => void;
 }) => (
   <div>
     <button
-      key={app.label}
+      key={label}
       className={
         "w-full border-2 border-gray-300 bg-inherit py-3 hover:bg-blue-100"
       }
       onClick={onClick}
     >
-      {app.back ? "<-" : ""} {app.label}
+      {back ? "<-" : ""} {label}
     </button>
   </div>
 );
