0d61819be35a9747a3905d34d4e4b4a8be99c064
Ryo-Kgym <mraytoh@icloud.com>
2024-03-24 23:36:47 +0900

    One-for-All-121 - Fixed the getCookieValue.

--- apps/web/src/persistence/browser/client/cookie.ts
+++ apps/web/src/persistence/browser/client/cookie.ts
@@ -5,5 +5,10 @@ export const saveCookie = ({ key, value }: { key: string; value: string }) => {
 };
 
 export const getCookieValue = <T>(key: string): T => {
-  return cookie.get(key) as T;
+  const stringValue = cookie.get(key);
+  if (!stringValue) {
+    throw new Error(`cookie not found: key=${key}`);
+  }
+
+  return JSON.parse(stringValue) as T;
 };
