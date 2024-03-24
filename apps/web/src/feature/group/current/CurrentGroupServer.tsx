add573f301f80b318f0501dab7c194c04c6d1b3d
Ryo-Kgym <mraytoh@icloud.com>
2024-03-24 23:56:17 +0900

    One-for-All-121 - Removed the group from SSC.

--- apps/web/src/feature/group/current/CurrentGroupServer.tsx
+++ apps/web/src/feature/group/current/CurrentGroupServer.tsx
@@ -1,8 +1,5 @@
 import { CurrentGroupClient } from "@feature/group/current/CurrentGroupClient";
-import { findUser } from "@persistence/browser/server/find-user";
 
 export const CurrentGroupServer = async () => {
-  const { group } = await findUser();
-
-  return <CurrentGroupClient groupName={group.name} />;
+  return <CurrentGroupClient />;
 };
