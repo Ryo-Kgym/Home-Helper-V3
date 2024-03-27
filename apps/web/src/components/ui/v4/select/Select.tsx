fcbcd1669629d21934e1f4df3c38357bf2781f88
Ryo-Kgym <mraytoh@icloud.com>
2024-03-27 23:50:12 +0900

    One-for-All-121 - Changed the Select design.

--- apps/web/src/components/ui/v4/select/Select.tsx
+++ apps/web/src/components/ui/v4/select/Select.tsx
@@ -17,22 +17,23 @@ export const Select = <T extends string | number>({
 
   return (
     <FieldContainer label={label}>
-      <input
-        className={`w-full cursor-pointer p-2 focus:outline-none disabled:cursor-not-allowed disabled:bg-gray-200 ${requiredBgColor}`}
-        value={selectedFieldName}
-        readOnly
-        onClick={() => setOpenOption(true)}
-        disabled={disabled}
-        placeholder={placeholder}
-      />
-      <div className={"absolute z-10 ml-10 bg-white"}>
+      <button onClick={() => setOpenOption(!openOption)}>
+        <input
+          className={`w-full cursor-pointer p-2 focus:outline-none disabled:cursor-not-allowed disabled:bg-gray-200 ${requiredBgColor}`}
+          value={selectedFieldName}
+          readOnly
+          disabled={disabled}
+          placeholder={placeholder}
+        />
+      </button>
+      <div className={"absolute z-10 ml-10 h-96 overflow-y-scroll bg-white"}>
         {data
           .filter((_) => openOption)
           .map((item) => (
             <div key={item.value}>
               <button
                 className={
-                  "w-full rounded-md border-2 border-gray-300 bg-inherit px-3 py-1 text-left hover:bg-gray-200"
+                  "w-full rounded-md border border-gray-300 bg-inherit px-3 py-1 text-left hover:bg-gray-200"
                 }
                 onClick={() => {
                   setValue(item.value);
