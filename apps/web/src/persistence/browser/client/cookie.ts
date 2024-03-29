37b1bbf05340510a40d10cded5ecf2e224029992
Ryo-Kgym <mraytoh@icloud.com>
2024-03-29 22:44:27 +0900

    One-for-All-121 - I added handling for when values couldn't be retrieved from cookies.

--- apps/web/src/persistence/browser/client/cookie.ts
+++ apps/web/src/persistence/browser/client/cookie.ts
@@ -4,10 +4,10 @@ export const saveCookie = ({ key, value }: { key: string; value: string }) => {
   cookie.set(key, value);
 };
 
-export const getCookieValue = <T>(key: string): T => {
+export const getCookieValue = <T>(key: string): T | null => {
   const stringValue = cookie.get(key);
   if (!stringValue) {
-    throw new Error(`cookie not found: key=${key}`);
+    return null;
   }
 
   return JSON.parse(stringValue) as T;
--- apps/web/src/persistence/browser/client/useFindUser.ts
+++ apps/web/src/persistence/browser/client/useFindUser.ts
@@ -1,6 +1,22 @@
 import type { User } from "@type/user";
+import { useEffect, useState } from "react";
 import { getCookieValue } from "@persistence/browser/client/cookie";
 
 export const useFindUser = (): User => {
-  return getCookieValue("user");
+  const [user, setUser] = useState<User>({
+    id: "",
+    name: "",
+    email: "",
+    group: { id: "", name: "" },
+  });
+
+  const optUser = getCookieValue("user");
+
+  useEffect(() => {
+    if (optUser) {
+      setUser(optUser as User);
+    }
+  }, []);
+
+  return user;
 };
