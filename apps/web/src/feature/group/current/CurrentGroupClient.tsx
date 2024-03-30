b3b47d186aabf85afa44e14c6494a10239423de5
Ryo-Kgym <mraytoh@icloud.com>
2024-03-30 13:40:20 +0900

    One-for-All-121 - I changed the implement.

--- apps/web/src/feature/group/current/CurrentGroupClient.tsx
+++ apps/web/src/feature/group/current/CurrentGroupClient.tsx
@@ -1,17 +1,23 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useFindUser } from "@persistence/browser/client/useFindUser";
 
 export const CurrentGroupClient = () => {
+  const [groupName, setGroupName] = useState<string>("Loading...");
   const { group } = useFindUser();
 
-  if (!group) return null;
+  useEffect(() => {
+    if (group.name) {
+      setGroupName(group.name);
+    }
+  }, [group.name]);
 
   return (
     <div className={"flex justify-end px-5 py-2"}>
       <div className={"flex space-x-5 text-xl"}>
         <div>選択中</div>
-        <div>{group.name}</div>
+        <div>{groupName}</div>
       </div>
     </div>
   );
