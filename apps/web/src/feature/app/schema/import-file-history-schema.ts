f48beb917247db05d04b94aa583d4038bfe1eed3
Ryo-Kgym <mraytoh@icloud.com>
2024-04-09 10:43:28 +0900

    One-for-All-131 - WIP

--- apps/web/src/feature/app/schema/import-file-history-schema.ts
+++ apps/web/src/feature/app/schema/import-file-history-schema.ts
@@ -1,7 +1,7 @@
 import { previewRecordsSchema } from "@feature/app/schema/preview-records-schema";
 import { z } from "zod";
 
-export const importFileHistorySchema = z.object({
+const importFileHistorySchema = z.object({
   id: z.string(),
   importDate: z.date(),
   fileName: z.string(),
--- apps/web/src/feature/app/schema/index.ts
+++ apps/web/src/feature/app/schema/index.ts
@@ -1,5 +1,5 @@
 export * from "./app-schema";
 export * from "./record-schema";
 export * from "./import-file-settings-schema";
-export * from "./import-file-history";
+export * from "./import-file-history-schema";
 export * from "./preview-records-schema";
