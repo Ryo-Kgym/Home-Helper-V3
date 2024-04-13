a1ca842c74cc5ba7c1e46d80aceaa5319c695b84
Ryo-Kgym <mraytoh@icloud.com>
2024-04-13 16:54:11 +0900

    One-for-All-131 - I enabled selecting fields for each app.

--- apps/web/src/feature/view/modify/ModifyViewClient.tsx
+++ apps/web/src/feature/view/modify/ModifyViewClient.tsx
@@ -1,9 +1,72 @@
 "use client";
 
-export const ModifyViewClient = ({ viewId }: { viewId: string }) => {
+import { useState } from "react";
+import { Select } from "@components/ui/v4/select";
+import { ViewFields } from "@oneforall/domain/schema/view/view-schema";
+
+type AppListData = {
+  label: string;
+  value: string;
+  fields: { label: string; value: string }[];
+}[];
+
+export const ModifyViewClient = ({
+  appListData,
+  viewFields,
+}: {
+  appListData: AppListData;
+  viewFields: ViewFields;
+}) => {
   return (
     <div>
-      <h1>ModifyView {viewId}</h1>
+      <div>
+        <ViewAppOption appListData={appListData} viewFields={viewFields} />
+      </div>
+    </div>
+  );
+};
+
+const ViewAppOption = ({
+  appListData,
+  viewFields,
+}: {
+  appListData: AppListData;
+  viewFields: ViewFields;
+}) => {
+  const [appId, setAppId] = useState<string>("");
+  const [appFieldObject, setAppFieldObject] = useState<
+    Record<keyof typeof viewFields, string>
+  >({});
+
+  const fieldListData =
+    appListData.find((app) => app.value === appId)?.fields ?? [];
+
+  return (
+    <div>
+      <div className={"flex"}>
+        <Select
+          data={appListData}
+          label={"アプリ"}
+          value={appId}
+          setValue={setAppId}
+        />
+        <div>
+          {Object.entries(viewFields).map(([viewFieldId, field]) => (
+            <Select
+              key={viewFieldId}
+              data={fieldListData}
+              label={field.fieldName}
+              value={appFieldObject[viewFieldId] ?? ""}
+              setValue={(value) =>
+                setAppFieldObject({
+                  ...appFieldObject,
+                  [viewFieldId]: value,
+                })
+              }
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- apps/web/src/feature/view/modify/ModifyViewServer.tsx
+++ apps/web/src/feature/view/modify/ModifyViewServer.tsx
@@ -1,5 +1,30 @@
 import { ModifyViewClient } from "@feature/view/modify/ModifyViewClient";
+import { convertToView } from "@feature/view/recordList/convert-to-view";
+import { findUser } from "@persistence/browser/server/find-user";
+import { fetchQuery } from "@persistence/database/server/fetchQuery";
+import {
+  GetAppFieldListDocument,
+  GetViewDocument,
+} from "@v3/graphql/public/type";
 
-export const ModifyViewServer = ({ viewId }: { viewId: string }) => {
-  return <ModifyViewClient viewId={viewId} />;
+export const ModifyViewServer = async ({ viewId }: { viewId: string }) => {
+  const { group } = await findUser();
+
+  const { data } = await fetchQuery(GetAppFieldListDocument, {
+    groupId: group.id,
+  });
+
+  const appListData =
+    data?.group?.apps.map((a) => ({
+      label: a.name,
+      value: a.id,
+      fields: a.fields.map((f) => ({ value: f.id, label: f.name })),
+    })) ?? [];
+
+  const { data: viewData } = await fetchQuery(GetViewDocument, { viewId });
+  const view = convertToView(viewData);
+
+  return (
+    <ModifyViewClient appListData={appListData} viewFields={view.fields} />
+  );
 };
