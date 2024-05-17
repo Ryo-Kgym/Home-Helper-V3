ae6b213b7da875a4d3abdb070ed35989ea648578
Ryo-Kgym <mraytoh@icloud.com>
2024-05-17 14:08:59 +0900

    One-for-All-144 - Copied from the template: https://github.com/expo/expo/tree/main/templates/expo-template-tabs

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -3,15 +3,43 @@ import type { ExpoConfig } from "@expo/config";
 const defineConfig = (): ExpoConfig => ({
   name: "Home-Helper",
   slug: "home-helper",
-  scheme: "home-helper",
   version: "0.1.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
-  userInterfaceStyle: "light",
+  scheme: "home-helper",
+  userInterfaceStyle: "automatic",
   splash: {
     image: "./assets/icon.png",
     resizeMode: "contain",
     backgroundColor: "#1F104A",
+  },
+  ios: {
+    // runtimeVersion: "1.0.0",
+    // bundleIdentifier: "your.bundle.identifier",
+    supportsTablet: true,
+  },
+  android: {
+    // runtimeVersion: {
+    //   policy: "appVersion",
+    // },
+    // package: "your.bundle.identifier",
+    adaptiveIcon: {
+      foregroundImage: "./assets/icon.png",
+      backgroundColor: "#1F104A",
+    },
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/icon.png",
+  },
+  plugins: [
+    "expo-router",
+    // "./expo-plugins/with-modify-gradle.js"
+  ],
+  experiments: {
+    tsconfigPaths: true,
+    typedRoutes: true,
   },
   updates: {
     fallbackToCacheTimeout: 0,
@@ -19,31 +47,11 @@ const defineConfig = (): ExpoConfig => ({
   },
   assetBundlePatterns: ["**/*"],
   runtimeVersion: "exposdk:49.0.0",
-  ios: {
-    runtimeVersion: "1.0.0",
-    bundleIdentifier: "your.bundle.identifier",
-    supportsTablet: true,
-  },
-  android: {
-    runtimeVersion: {
-      policy: "appVersion",
-    },
-    package: "your.bundle.identifier",
-    adaptiveIcon: {
-      foregroundImage: "./assets/icon.png",
-      backgroundColor: "#1F104A",
-    },
-  },
   extra: {
     eas: {
       projectId: "86b12fab-9446-4883-bde4-cac8a047cdaa",
     },
   },
-  experiments: {
-    tsconfigPaths: true,
-    typedRoutes: true,
-  },
-  plugins: ["expo-router", "./expo-plugins/with-modify-gradle.js"],
 });
 
 export default defineConfig;
---
+++ apps/expo/src/index.tsx
@@ -0,0 +1,4 @@
+// index.js or index.tsx
+import App from "./app";
+
+export default App;
