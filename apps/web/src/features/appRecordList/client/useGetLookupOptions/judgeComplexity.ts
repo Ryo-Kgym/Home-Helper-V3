0f926f0998665c552fee18120e47761f2c59066f
Ryo-Kgym <mraytoh@icloud.com>
2024-05-25 14:11:25 +0900

    One-for-All-140 Implemented the judgeComplexity.

---
+++ apps/web/src/features/appRecordList/client/useGetLookupOptions/judgeComplexity.ts
@@ -0,0 +1,19 @@
+import { FilterComplexity } from "@oneforall/domain/schema/filterComplexitySchema";
+
+export const judgeComplexity = (
+  source: { complexity: FilterComplexity; result: boolean }[],
+): boolean => {
+  let ret = source[0]?.result ?? true;
+
+  const sourceSecond = source.slice(1);
+
+  for (const { complexity, result } of sourceSecond) {
+    if (complexity === "and") {
+      ret = ret && result;
+    } else {
+      ret = ret || result;
+    }
+  }
+
+  return ret;
+};
