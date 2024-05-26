ec9b3c339ef74b4f96e0dfc4ce67aee749e585b7
Ryo-Kgym <mraytoh@icloud.com>
2024-05-27 00:24:30 +0900

    One-for-All-152 Called the getLookupRecords

--- apps/web/src/pageComponents/appRecordImport/hooks/useInsertImportFileRecords/convertToRecordObjects.ts
+++ apps/web/src/pageComponents/appRecordImport/hooks/useInsertImportFileRecords/convertToRecordObjects.ts
@@ -3,6 +3,7 @@ import {
   PreviewRecords,
 } from "@oneforall/domain/schema/previewRecordsSchema";
 import { recordColumnSchema } from "@oneforall/domain/schema/recordSchema";
+import { LookupRecords } from "@server/lookupRecords/type";
 import { RecordInsertInput } from "@v3/graphql/public";
 
 /**
@@ -13,25 +14,36 @@ export const convertToRecordObjects = (
   previewRecords: PreviewRecords,
   recordIds: string[],
   currentMaxIndex: number,
+  lookupRecords: LookupRecords,
 ): RecordInsertInput[] =>
   Object.values(previewRecords).map(({ columns }, index) => ({
     id: recordIds[index],
     appId,
     columns: Object.fromEntries(
-      Object.entries(columns).map(([fieldId, column]) => [
-        fieldId,
-        { ...recordColumnSchema.parse(column), ...optionsAttribute(column) },
-      ]),
+      Object.entries(columns).map(([fieldId, column]) => {
+        const lookupRecord = lookupRecords[fieldId] ?? {};
+
+        return [
+          fieldId,
+          {
+            ...recordColumnSchema.parse(column),
+            ...optionsAttribute(column, lookupRecord),
+          },
+        ];
+      }),
     ),
     index: currentMaxIndex + index + 1,
   }));
 
-const optionsAttribute = (column: PreviewRecordColumn) => {
+const optionsAttribute = (
+  column: PreviewRecordColumn,
+  lookupRecord: Record<string, string>,
+) => {
   switch (column.fieldKind) {
     case "lookup": {
       return {
         options: {
-          label: column.value,
+          label: lookupRecord[column.value ?? ""] ?? "",
         },
       };
     }
