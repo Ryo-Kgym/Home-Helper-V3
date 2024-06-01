5a0d6fc996e1efbb0789ed06723583ee705840c3
Ryo-Kgym <mraytoh@icloud.com>
2024-06-02 00:01:28 +0900

    One-for-All-156 WIP sumRecords.ts

---
+++ packages/domain/convert/summary/sumRecords.ts
@@ -0,0 +1,12 @@
+import { Records } from "../../schema/recordSchema";
+import {
+  SummaryCriteria,
+  SumRecords,
+} from "../../schema/summary/sumRecordsSchema";
+
+export const sumRecords = (
+  records: Records,
+  criteria: SummaryCriteria,
+): SumRecords => {
+  return {};
+};
