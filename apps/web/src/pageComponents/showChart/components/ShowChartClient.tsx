1c804ae2f7f6498300bc695c43ac40416a9644f4
Ryo-Kgym <mraytoh@icloud.com>
2024-06-03 22:57:45 +0900

    One-for-All-149 Separated the components.

--- apps/web/src/pageComponents/showChart/components/ShowChartClient.tsx
+++ apps/web/src/pageComponents/showChart/components/ShowChartClient.tsx
@@ -1,64 +1,18 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { ComponentProps } from "react";
 import { Title } from "@components/ui/v4/frame/Title";
-import { Select } from "@components/ui/v4/select";
-import BarChart from "@features/chart/components/BarChart";
-import LineChart from "@features/chart/components/LineChart";
-import PieChart from "@features/chart/components/PieChart";
-import RadarChart from "@features/chart/components/RadarChart";
+
+import { SwitchingCharts } from "./SwitchingCharts";
 
 const ShowChartClient = ({
   data,
   colors,
-}: {
-  data: { name: string; value: number }[];
-  colors: string[];
-}) => {
-  const [selectedOption, setSelectedOption] = useState<string>("pieChart");
-
-  const renderContent = () => {
-    switch (selectedOption) {
-      case "pieChart":
-        return (
-          <PieChart
-            data={data}
-            colors={colors}
-            outerRadius={150}
-            width={400}
-            height={400}
-          />
-        );
-      case "barChart":
-        return (
-          <BarChart data={data} colors={colors} width={"30%"} height={400} />
-        );
-      case "lineChart":
-        return (
-          <LineChart data={data} colors={colors} width={600} height={400} />
-        );
-      case "radarChart":
-        return <RadarChart data={data} />;
-    }
-  };
-
-  const selectData = [
-    { value: "pieChart", label: "PieChart" },
-    { value: "barChart", label: "BarChart" },
-    { value: "lineChart", label: "LineChart" },
-    { value: "radarChart", label: "RadarChart" },
-  ];
-
+}: ComponentProps<typeof SwitchingCharts>) => {
   return (
     <div className={"space-y-4"}>
-      <Title title={"chart作成画面"} />
-      <Select
-        data={selectData}
-        label={"Select Charts"}
-        value={selectedOption}
-        setValue={setSelectedOption}
-      />
-      <div>{renderContent()}</div>
+      <Title title={"グラフ表示"} />
+      <SwitchingCharts data={data} colors={colors} />
     </div>
   );
 };
