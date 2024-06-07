3db4edb370ca76677aa3f7cfdfa9e8f67a8aebc1
Ryo-Kgym <mraytoh@icloud.com>
2024-06-07 23:05:51 +0900

    One-for-All-160 WIP Tested the parseToViewRecordColumns.

---
+++ packages/graphql/public/convert/parseToViewRecords/parseToViewRecordColumns.test.ts
@@ -0,0 +1,73 @@
+import { ViewFields } from "@oneforall/domain/schema/view/viewSchema";
+
+import { parseToViewRecordColumns } from "./parseToViewRecordColumns";
+
+describe("parseToViewRecordColumns", () => {
+  const viewFields: ViewFields = {
+    vf1: {
+      id: "vf1",
+      fieldKind: "text",
+      options: {},
+      fieldIndex: 0,
+      fieldName: "fieldName1",
+    },
+  };
+
+  it("パースに成功した場合", () => {
+    const actual = parseToViewRecordColumns(
+      viewFields,
+      {
+        vf1: {
+          appFieldId: "af1",
+        },
+      },
+      {
+        id: "r1",
+        index: 0,
+        columns: {
+          af1: {
+            fieldKind: "text",
+            value: "value",
+            options: {
+              label: "label",
+            },
+          },
+        },
+      },
+    );
+
+    expect(actual).toEqual({
+      vf1: {
+        fieldKind: "text",
+        value: "value",
+        options: {
+          label: "label",
+        },
+      },
+    });
+  });
+
+  it("パースに失敗した場合", () => {
+    const actual = parseToViewRecordColumns(
+      viewFields,
+      {
+        vf1: {
+          appFieldId: "af1",
+        },
+      },
+      {
+        id: "r1",
+        index: 0,
+        columns: {}, // データがない
+      },
+    );
+
+    expect(actual).toEqual({
+      vf1: {
+        fieldKind: "text",
+        value: "",
+        options: {},
+      },
+    });
+  });
+});
