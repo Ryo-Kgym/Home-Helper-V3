2842553238abe162540d54417379abff22fe5097
Ryo-Kgym <mraytoh@icloud.com>
2024-06-13 12:37:09 +0900

    One-for-All-165 Used the tabs.

--- apps/web/src/app/(appListNavigation)/view/modify/[id]/page.tsx
+++ apps/web/src/app/(appListNavigation)/view/modify/[id]/page.tsx
@@ -1,7 +1,49 @@
+import { FC } from "react";
+import { RoutingTabs } from "@components/ui/v4/routingTab";
 import { ModifyView } from "@features/viewAppModify";
 
-const Page = ({ params: { id } }: { params: { id: string } }) => (
-  <ModifyView viewId={id} />
-);
+const Page = ({
+  params: { id },
+  searchParams,
+}: {
+  params: { id: string };
+  searchParams: { tab: string | undefined };
+}) => {
+  const tabGroup: Record<
+    string,
+    {
+      label: string;
+      Component: FC<{ viewId: string }>;
+    }
+  > = {
+    viewApp: {
+      label: "構成するアプリの変更",
+      Component: ModifyView,
+    },
+    fields: {
+      label: "フィールドの変更",
+      Component: () => <div>Fieldsの編集</div>,
+    },
+  };
+
+  const { Component } = tabGroup[searchParams.tab ?? "viewApp"] ?? {
+    Component: NotFound,
+    label: "404",
+  };
+
+  return (
+    <div>
+      <RoutingTabs
+        tabs={Object.fromEntries(
+          Object.entries(tabGroup).map(([k, v]) => [k, { label: v.label }]),
+        )}
+        currentTab={searchParams.tab ?? "viewApp"}
+      />
+      <Component viewId={id} />
+    </div>
+  );
+};
 
 export default Page;
+
+const NotFound = (_: { viewId: string }) => <div>404</div>;
