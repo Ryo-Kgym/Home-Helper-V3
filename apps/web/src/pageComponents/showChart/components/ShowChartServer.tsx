0472c7846b0f6563bb93bd408aff115abf447f8f
Ryo-Kgym <mraytoh@icloud.com>
2024-06-13 23:45:00 +0900

    One-for-All-156 WIP

--- apps/web/src/pageComponents/showChart/components/ShowChartServer.tsx
+++ apps/web/src/pageComponents/showChart/components/ShowChartServer.tsx
@@ -1,6 +1,36 @@
+import { SummaryCriteria } from "@oneforall/domain/schema/summary/sumRecordsSchema";
 import ShowChartClient from "@pageComponents/showChart/components/ShowChartClient";
 
 export const ShowChartServer = async () => {
+  // レコードデータの呼び出しは RecordListServer を参照
+
+  // 条件はこれを使用する
+  const criteria: SummaryCriteria = {
+    groupingFields: {
+      "1718289203212": {
+        id: "1718289203212",
+        fieldName: "値",
+        fieldKind: "text",
+        fieldIndex: 1,
+        options: {},
+      },
+    },
+    summaryFields: {
+      "1718289203213": {
+        id: "1718289203213",
+        fieldName: "数値",
+        fieldKind: "numeric",
+        fieldIndex: 2,
+        options: {
+          thousandsSeparatorPosition: 3,
+        },
+      },
+    },
+  };
+  criteria;
+
+  // 上記を組み合わせて、data を生成する
+
   const data = [
     { name: "Group A", value: 400 },
     { name: "Group B", value: 300 },
