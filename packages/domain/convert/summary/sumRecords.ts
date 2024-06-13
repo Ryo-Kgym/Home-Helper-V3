92faf55cbb61dd75beee0f8885a82591e96d70ed
Ryo-Kgym <mraytoh@icloud.com>
2024-06-13 23:09:15 +0900

    One-for-All-156 WIP

--- packages/domain/convert/summary/sumRecords.ts
+++ packages/domain/convert/summary/sumRecords.ts
@@ -1,88 +1,47 @@
-import { Record, Records } from "../../schema/recordSchema";
-import {
-  SummaryCriteria,
-  SumRecords,
-} from "../../schema/summary/sumRecordsSchema";
+import { Records } from "../../schema/recordSchema";
+import { SummaryCriteria } from "../../schema/summary/sumRecordsSchema";
 
 export const sumRecords = (records: Records, criteria: SummaryCriteria) => {
+  if (
+    Object.keys(criteria.groupingFields).length < 1 ||
+    Object.keys(criteria.summaryFields).length < 1
+  ) {
+    throw new Error("groupingFields is empty");
+  }
+
   const groupingFieldId = Object.values(criteria.groupingFields)[0]!.id;
-  const summaryFieldId = Object.values(criteria.summaryFields)[0]!.id;
-  console.log(groupingFieldId);
-  console.log(summaryFieldId);
+  const sumFieldId = Object.values(criteria.summaryFields)[0]!.id;
 
-  records.map((record: Record, index: number)
-    =>
-  {
-    const groupingField = record.columns.groupingFieldId?.value;
-    console.log(groupingField)
-    const summaryField = record.columns.summaryFieldId?.value;
-    let sum = 0;
-    let count = 1;
-    let i = 0;
+  const list = Object.values(records).map(
+    (r) => r.columns[groupingFieldId]?.value,
+  );
 
-    records.filter(i => i !== index).map((filterdRecord: Record, filterdIndex: number) => {
-      if (filterdRecord.columns.groupingFieldId?.value === groupingField) {
-        sum = sum + Number(filterdRecord.columns.summaryFieldId?.value) + Number(summaryField);
-        count++;
-      }
+  const uniqueGroupingValues = [...new Set(list)];
 
-      if (i < records.length) {
-        i = i + 1;
-      }
-      // ここで配列にプッシュして結果を保持？
-      // recordList.push
-    })
-  })
+  const result = uniqueGroupingValues
+    .map((groupingValue) => ({
+      groupingFieldId,
+      groupingValue,
+      sumFieldId,
+    }))
+    .flatMap((field) => {
+      const motherRecords = Object.values(records).filter(
+        (record) =>
+          record.columns[field.groupingFieldId]?.value === field.groupingValue,
+      );
 
-  // const recordList = (records: Records) => {
-  //   // const recordList = [];
-  //   records?.map((record, index: number)
-  // =>
-  //   {
-  //     const groupingField = record.columns.groupingFieldId.value;
-  //     const summaryField = record.columns.summaryFieldId.value;
-  //     let sum = 0;
-  //     let count = 1;
-  //
-  //     record.filter((_, i) => i !== index).map((filterdRecord, filterdIndex: number) => {
-  //       if (filterdRecord.columns.groupingFieldId.value === groupingField) {
-  //         sum = Number(filterdRecord.columns.summaryFieldId.value) + Number(summaryField);
-  //         count++;
-  //       }
-  //       // ここで配列にプッシュして結果を保持？
-  //       // recordList.push
-  //     })
-  //   })}
+      const sum = motherRecords.reduce((acc, record) => {
+        return acc + Number(record.columns[field.sumFieldId]?.value);
+      }, 0);
 
-  const result = [
-    {
-      groupingFieldId: "c1",
-      groupingValue: "grouping1",
-      sumFieldId: "c2",
-    },
-    {
-      groupingFieldId: "c1",
-      groupingValue: "grouping2",
-      sumFieldId: "c2",
-    },
-  ].flatMap((field) => {
-    const motherRecords = Object.values(records).filter(
-      (record) =>
-        record.columns[field.groupingFieldId]?.value === field.groupingValue,
-    );
-
-    const sum = motherRecords.reduce((acc, record) => {
-      return acc + Number(record.columns[field.sumFieldId]?.value);
-    }, 0);
-
-    return [
-      {
-        sum,
-        count: motherRecords.length,
-        average: sum / motherRecords.length,
-      },
-    ];
-  });
+      return [
+        {
+          sum,
+          count: motherRecords.length,
+          average: sum / motherRecords.length,
+        },
+      ];
+    });
 
   return Object.fromEntries(result.map((res, index) => [index, res]));
 };
