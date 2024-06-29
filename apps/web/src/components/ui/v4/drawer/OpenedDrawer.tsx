39c26969853dc7f603badfbe5429f56e13619e04
Ryo-Kgym <mraytoh@icloud.com>
2024-06-29 16:53:27 +0900

    One-for-All-172 Implemented the OpenedDrawer.

---
+++ apps/web/src/components/ui/v4/drawer/OpenedDrawer.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { PropsWithChildren } from "react";
+import { useRouter } from "next/navigation";
+import { Drawer } from "@components/ui/v4/drawer/index";
+
+export const OpenedDrawer = ({ children }: PropsWithChildren) => {
+  const { back } = useRouter();
+
+  return (
+    <Drawer opened={true} onClose={back}>
+      {children}
+    </Drawer>
+  );
+};
