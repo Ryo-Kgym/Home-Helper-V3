ba2603f083439bc118173f68bc27f8433651d3a6
Ryo-Kgym <mraytoh@icloud.com>
2024-07-19 12:41:47 +0900

    One-for-All-178 Implemented the not-found page.

---
+++ apps/web/src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { paths } from "@routing/paths";
+
+const NotFound = () => {
+  return (
+    <div className={"flex h-[100vh] items-center justify-center"}>
+      <div>
+        <div className={"text-xl"}>Not Found</div>
+        <Link href={paths.group.select}>グループ選択へ戻る</Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
