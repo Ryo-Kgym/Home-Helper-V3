dcdbe4893b973f005a65e3f977a0135071eaa515
Ryo-Kgym <mraytoh@icloud.com>
2024-12-07 15:51:20 +0900

    One-for-All-190 月毎のカテゴリ別収支のタイトルを表示した。

--- apps/web/src/features/householdDashboard/components/HouseholdDashboardClient.tsx
+++ apps/web/src/features/householdDashboard/components/HouseholdDashboardClient.tsx
@@ -17,11 +17,29 @@ export const HouseholdDashboardClient: FC<Props> = ({ items }) => {
   return (
     <div className={styles.module}>
       {items.map((item) => (
-        <ItemCard key={item.dashboardSettingId} itemName={item.feature}>
+        <ItemCard key={item.dashboardSettingId} itemName={buildItemName(item)}>
           {item.feature === "balance" && <DashboardBalanceClient {...item} />}
           {item.feature === "monthly" && <DashboardMonthlyClient {...item} />}
         </ItemCard>
       ))}
     </div>
   );
+};
+
+const buildItemName = (params: Props["items"][number]) => {
+  switch (params.feature) {
+    case "monthly": {
+      const month =
+        Number(
+          params.dashboardSettingArgs.find((arg) => arg.type === "month")
+            ?.value,
+        ) ?? 0;
+      const targetDate = new Date();
+      targetDate.setMonth(targetDate.getMonth() + month);
+
+      return `${targetDate.getFullYear()}年${targetDate.getMonth() + 1}月の実績`;
+    }
+    default:
+      return params.feature;
+  }
 };
