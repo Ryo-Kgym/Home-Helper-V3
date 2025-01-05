cc3bbec290c7db138a35bc7e7ce4210be7fe6b22
Ryo-Kgym <mraytoh@icloud.com>
2025-01-05 23:45:38 +0900

    One-for-All-219 登録に使用するマスタデータを取得するようにした。

--- apps/web/src/features/householdRegisterDaily/components/RegisterDailyButtonServer.tsx
+++ apps/web/src/features/householdRegisterDaily/components/RegisterDailyButtonServer.tsx
@@ -1,5 +1,16 @@
+import { GetDetailMasterDocument } from "@v3/graphql/household/schema/query/v5/queryDetailMaster.generated";
+
+import { findUser } from "../../../persistence/browser/server/find-user";
+import { execQuery } from "../../../persistence/database/server/execQuery";
 import { RegisterDailyButton } from "./RegisterDailyButton";
 
 export const RegisterDailyButtonServer = async () => {
+  const { group } = await findUser();
+
+  const { data } = await execQuery(GetDetailMasterDocument, {
+    groupId: group.id,
+  });
+  console.log(data);
+
   return <RegisterDailyButton />;
 };
