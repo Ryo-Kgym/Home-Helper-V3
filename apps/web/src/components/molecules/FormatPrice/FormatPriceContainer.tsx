cae12bc56bc3e21c49f637632eaab3d2a2617e68
Ryo-Kgym <mraytoh@icloud.com>
2025-02-03 22:27:19 +0900

    One-for-All-240 不要なモジュールを削除した。

--- apps/web/src/components/molecules/FormatPrice/FormatPriceContainer.tsx
+++ apps/web/src/components/molecules/FormatPrice/FormatPriceContainer.tsx
@@ -3,7 +3,6 @@
  */
 
 import { IocomeType } from "../../../domain/model/household/IocomeType";
-import { FormatPricePresenter } from "./FormatPricePresenter";
 
 export const FormatPriceContainer = ({
   iocomeType,
@@ -16,12 +15,14 @@ export const FormatPriceContainer = ({
 
   const priceWithComma = Number(price).toLocaleString();
 
-  const className =
-    iocomeType === IocomeType.Income ? "text-green-600" : "text-red-400";
-  const priceStr =
-    iocomeType === IocomeType.Income
-      ? priceWithComma
-      : "(" + priceWithComma + ")";
-
-  return <FormatPricePresenter className={className} priceStr={priceStr} />;
+  return (
+    <div
+      style={{
+        fontWeight: "bold",
+        color: iocomeType === IocomeType.Income ? "green" : "red",
+      }}
+    >
+      {priceWithComma}
+    </div>
+  );
 };
